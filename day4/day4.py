d92d995843bdcf1062c9f8a0c676f3ca88d429c7
Simon Muller <samullers@gmail.com>
2020-12-04 09:53:47 +0200

    Move field checks into separate functions

--- day4/day4.py
+++ day4/day4.py
@@ -3,6 +3,76 @@ import sys; sys.path.append("..")
 from lib import prod, Map2D, wrap
 import re
 
+
+def byr(val):
+	# byr (Birth Year) - four digits; at least 1920 and at most 2002.
+	val = int(val)
+	if not (1920 <= val <= 2002):
+		return False
+	return True
+
+def iyr(val):
+	# iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+	val = int(val)
+	if not (2010 <= val <= 2020):
+		return False
+	return True
+
+def eyr(val):
+	# eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+	val = int(val)
+	if not (2020 <= val <= 2030):
+		return False
+	return True
+
+def hgt(val):
+	# hgt (Height) - a number followed by either cm or in:
+	#     If cm, the number must be at least 150 and at most 193.
+	#     If in, the number must be at least 59 and at most 76.
+	if len(val) <= 2:
+		return False
+	unit = val[-2:]
+	if unit not in ['in', 'cm']:
+		return False
+	val = int(val[:-2])
+	if unit == 'cm' and not (150 <= val <= 193):
+		return False
+	if unit == 'in' and not (59 <= val <= 76):
+		return False
+	return True
+
+def hcl(val):
+	# hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+	# if len(val) != 7 or val[0] != '#':
+	# 	return False
+	if not re.match("^#[a-f0-9]{6}$", val):
+		return False
+	return True
+
+def ecl(val):
+	# ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+	if val not in ['amb','blu','brn','gry','grn','hzl','oth']:
+		return False
+	return True
+
+def pid(val):
+	# pid (Passport ID) - a nine-digit number, including leading zeroes.
+	if len(val) != 9 or not re.match("^[0-9]+$", val):
+		return False
+	return True
+
+def cid(val):
+	# cid (Country ID) - ignored, missing or not.
+	return True
+
+
+def check_field_value(key, val):
+	# Dirty switch-case/dict-to-func workaround
+	if not eval(key + '(val)'):
+		return False
+	return True
+
+
 # ['byr','iyr','eyr','hgt','hcl','ecl','pid','cid']
 def check_passport(full_fields, debug=False):
 	fields_found = []
@@ -12,53 +82,8 @@ def check_passport(full_fields, debug=False):
 		assert(len(key) == 3)
 		fields_found.append(key)
 
-		# byr (Birth Year) - four digits; at least 1920 and at most 2002.
-		if key == 'byr':
-			val = int(val)
-			if not (1920 <= val <= 2002):
-				return False
-		# iyr (Issue Year) - four digits; at least 2010 and at most 2020.
-		if key == 'iyr':
-			val = int(val)
-			if not (2010 <= val <= 2020):
-				return False
-		# eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
-		if key == 'eyr':
-			val = int(val)
-			if not (2020 <= val <= 2030):
-				return False
-		# hgt (Height) - a number followed by either cm or in:
-		#     If cm, the number must be at least 150 and at most 193.
-		#     If in, the number must be at least 59 and at most 76.
-		if key == 'hgt':
-			if len(val) <= 2:
-				return False
-			unit = val[-2:]
-			if unit not in ['in', 'cm']:
-				return False
-			val = int(val[:-2])
-			if unit == 'cm' and not (150 <= val <= 193):
-				return False
-			if unit == 'in' and not (59 <= val <= 76):
-				return False
-		# hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
-		if key == 'hcl':
-			# if len(val) != 7 or val[0] != '#':
-			# 	return False
-			if not re.match("^#[a-f0-9]{6}$", val):
-				return False
-		# ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
-		if key == 'ecl':
-			if val not in ['amb','blu','brn','gry','grn','hzl','oth']:
-				return False
-		# pid (Passport ID) - a nine-digit number, including leading zeroes.
-		if key == 'pid':
-			if len(val) != 9 or not re.match("^[0-9]+$", val):
-				print(val)
-				return False
-		# cid (Country ID) - ignored, missing or not.
-		if key == 'cid':
-			pass
+		if not check_field_value(key, val):
+			return False
 
 	if 'cid' in fields_found:
 				fields_found.remove('cid') #optional
@@ -76,7 +101,7 @@ def check_passport(full_fields, debug=False):
 	return False
 
 
-# 181, 182 / 110, 112, 111 (5min), 109
+# 181, 182 / 110, 110, 112, 111 (5min), 109
 # bugs:
 # - part 1: last count
 # - part 2: iyr copy-paste range / hgt elif unit / hcl a-z / pid and/or
@@ -107,5 +132,3 @@ if __name__ == '__main__':
 	if check_passport(curr_full_fields):
 		valid_passports += 1
 	print(valid_passports)
-
-
