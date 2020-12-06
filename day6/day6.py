9d4a3c4bb05844250f0de3da661d96d7f52fd86f
Simon Muller <samullers@gmail.com>
2020-12-06 07:51:15 +0200

    Use proper set commands

--- day6/day6.py
+++ day6/day6.py
@@ -22,11 +22,12 @@ a
 
 b
 	"""
-	# internet connection...
-	# 6567, 6585? (server @ 7:11, @ 7:14) / 3264 (0729)
+	# 6567, 6585 (server @ 7:11, @ 7:14) / 3264 (0729), 3276
+	# part 1: internet connection... and then missed seeing part 2
+	# part 2: missed 1 line long groups
 	lines = [line.strip() for line in input_file.readlines()] #test]
 	# lines = test.split('\n')
-	lines.append('\n')
+	lines.append('')
 	print('Lines: {}'.format(len(lines)))
 	
 
@@ -37,41 +38,35 @@ b
 	total_quest = 0
 	total_quest_2 = 0
 	for line in lines:
-		print(''.join(sorted(line)))
-		if line.strip() == '':
+		# print(''.join(sorted(line)))
+		if line == '':
+			# new group
+			# print('[1]:', curr_group, len(curr_group), total_quest)
 			curr_ques_count = len(curr_group)
 			total_quest += curr_ques_count
-			total_quest_2 += len(curr_group_2)
-			# print('[1]:', curr_group, len(curr_group), total_quest)
-			# new group
-			print('end', ''.join(sorted(curr_group_2)))
-			print('[2]:', curr_group_2, len(curr_group_2), total_quest_2)
-			line_in_group = 0
 			curr_group = set()
+
+			# print('end', ''.join(sorted(curr_group_2)))
+			# print('[2]:', curr_group_2, len(curr_group_2), total_quest_2)
+			total_quest_2 += len(curr_group_2)
+			line_in_group = 0		
 			curr_group_2 = set()
 		else:
-			tmp_curr_group_2 = set()
-			for c in line.strip():
-				curr_group.add(c)
-				tmp_curr_group_2.add(c)
+			curr_group = curr_group.union(set(line))
+			tmp_curr_group_2 = set(line)
 			if len(curr_group_2) == 0 and line_in_group == 0:
 				curr_group_2 = tmp_curr_group_2
 			else:
-				# curr_group_2 -= tmp_curr_group_2
-				eval_curr_group_2 = curr_group_2.copy()
-				for c in curr_group_2:
-					if c not in tmp_curr_group_2:
-						eval_curr_group_2.remove(c)
-				curr_group_2 = eval_curr_group_2
+				curr_group_2 = curr_group_2.intersection(tmp_curr_group_2)
 			line_in_group += 1
 	curr_ques_count = len(curr_group)
 	total_quest += curr_ques_count
 	# print(curr_group, len(curr_group), total_quest)
-	# print(total_quest)
+	print(total_quest)
 
 	total_quest_2 += len(curr_group_2)
-	print('end-fin', ''.join(sorted(curr_group_2)))
-	print('[2]:', curr_group_2, len(curr_group_2), total_quest_2)
+	# print('end-fin', ''.join(sorted(curr_group_2)))
+	# print('[2]:', curr_group_2, len(curr_group_2), total_quest_2)
 	print(total_quest_2)
 
 
