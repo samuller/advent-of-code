70a30905abcf3ff43a65b8a862f55dc53fdad4c3
Simon Muller <samullers@gmail.com>
2020-12-07 18:07:18 +0200

    Use common function to parse input into dictionary

--- day7/day7.py
+++ day7/day7.py
@@ -4,76 +4,36 @@ import sys; sys.path.append("..")
 from lib import *
 
 
-def look_for(input, iwant, known_containers=None):
-	# print('look_for( input,', iwant + ',', known_containers, ')')
+def look_for(tree, iwant, known_containers=None):
+	# print('look_for( tree,', iwant + ',', known_containers, ')')
 	if known_containers is None:
 		known_containers = set()
 
 	containers = set()
-	for line in input:
-		line = line.replace(' bags.', '')
-		line = line.replace(' bags', '')
-		line = line.replace(' bag', '')
+	for big in tree:
+		inside = tree[big]
 
-		fields = line.split(' contain ')
-		assert len(fields) == 2, fields
-		big, inside = fields
-
-		inside = inside.split(', ')
-		inside = [
-			(int(ins.split(' ')[0]) if ins.split(' ')[0] != 'no' else 0,
-				' '.join(ins.split(' ')[1:])
-			) for ins in inside
-		]
-		counts = [int(ins[0]) if ins[0] != 'no' else 0 for ins in inside]
-		# print(inside)
-
-		# assert len(inside) == 2, len(inside)
-		#if  big == iwant
 		for ins in inside:
 			if iwant in ins[1]:
 				containers.add(big)
-				# print()
-				# print(ins)
-				# print(big)
-				# print(inside)
-				# print(counts)
-				# print()
+
 	if len(containers) == 0:
 		return []
-	# print(len(known_containers))
-	# print(containers)
-	# print(known_containers)
+
 	for new_want in containers:
-		super_containers = look_for(input, new_want, containers)
+		super_containers = look_for(tree, new_want, containers)
 		containers = containers.union(super_containers)
 	return containers
 
 
-def count_inside(input, look_inside):
-	# print()
-	# print('look_for( input,', look_inside, ')')
-
+def count_inside(tree, look_inside):
+	# print('look_for( tree,', look_inside, ')')
 	containers = set()
 	total_count = 0
 	bigs = set()
-	for line in input:
-		line = line.replace(' bags.', '')
-		line = line.replace(' bags', '')
-		line = line.replace(' bag.', '')
-		line = line.replace(' bag', '')
-
-		fields = line.split(' contain ')
-		assert len(fields) == 2, fields
-		big, inside = fields
-
-		inside = inside.split(', ')
-		inside = [
-			(int(ins.split(' ')[0]) if ins.split(' ')[0] != 'no' else 0,
-				' '.join(ins.split(' ')[1:])
-			) for ins in inside
-		]
-		counts = [int(ins[0]) if ins[0] != 'no' else 0 for ins in inside]
+	for big in tree:
+		inside = tree[big]
+		counts = [ins[0] for ins in inside]
 		if big == look_inside:
 			total_count += sum(counts)
 			for ins in inside:
@@ -85,15 +45,39 @@ def count_inside(input, look_inside):
 	for inss in containers:
 		count = inss[0]
 		new_look = inss[1]
-		sub_count = count_inside(input, new_look)
+		sub_count = count_inside(tree, new_look)
 		# print(look_inside, total_count, '+ (', sub_count, '*', count, new_look, ')')
 		total_count += (count * sub_count)
 	# print(look_inside, 'total:', total_count)
 	return total_count
 
 
+def parse_tree(input_lines):
+	bags = {}
+	for line in lines:
+		assert line[-1] == '.'
+		# Remove full stop at end of each sentence
+		line = line[:-1]
+
+		fields = line.split(' contain ')
+		assert len(fields) == 2, fields
+		big, inside = fields
+		# Drop last word of description (either 'bag' or 'bags')
+		big = ' '.join(big.split(' ')[:-1])
+
+		bags[big] = []
+		for ins in inside.split(', '):
+			words = ins.split(' ')
+			count = 0 if words[0] == 'no' else int(words[0])
+			# Get descriptive adjectives (ignoring last word which is 'bag' or 'bags')
+			desc = ' '.join(words[1:-1])
+			bags[big].append((count, desc))
+	return bags
+
+
 # 93873 @ 7:50, 158493 @ 8:12
-# forgot "bag."
+# part 1: infinite recursion -> reduction -> previously seen values
+# part 2: multiplication, forgot "bag."
 if __name__ == '__main__':
 	lines = """light red bags contain 1 bright white bag, 2 muted yellow bags.
 dark orange bags contain 3 bright white bags, 4 muted yellow bags.
@@ -115,9 +99,11 @@ dotted black bags contain no other bags."""
 	lines = [line.strip() for line in fileinput.input('input.txt')]
 	print('Lines: {}'.format(len(lines)))
 
+	tree = parse_tree(lines)
+
 	iwant = 'shiny gold'
-	needs = look_for(lines, iwant)
+	needs = look_for(tree, iwant)
 	print('Bags containing:', len(needs))
 
-	min_bags_needed = count_inside(lines, iwant)
+	min_bags_needed = count_inside(tree, iwant)
 	print('Bags inside:', min_bags_needed)
