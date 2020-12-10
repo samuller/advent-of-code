3c2c98f52bb0d8448d8ae224e058aeb68c14b435
Simon Muller <samullers@gmail.com>
2020-12-10 10:00:16 +0200

    Day 10 part 1 and 2

---
+++ day10/day10.py
@@ -0,0 +1,257 @@
+#!/usr/bin/env python3
+import fileinput
+import sys; sys.path.append("..")
+from lib import *
+import numpy as np
+
+
+def can_connect(main, others):
+	valid = []
+	idxs = []
+	for idx, other in enumerate(others):
+		# print(other - main)
+		if 0 < (other - main) <= 3: #in [1, 3]:
+			valid.append(other)
+			idxs.append(idx)
+	return valid, idxs
+
+
+def find_arrangement(adapters):
+	adapters = list(adapters)
+	curr_adapter = 0
+	chosen_order = []
+	diffs = []
+	options = []
+	while len(adapters) > 0:
+		found, _ = can_connect(curr_adapter, adapters)
+		# print(found)
+		options.append(len(found))
+		print(found)
+		chosen = min(found)
+		diffs.append(chosen - curr_adapter)
+		adapters.remove(chosen)
+		curr_adapter = chosen
+		chosen_order.append(chosen)
+	return chosen_order, diffs, options
+
+
+# [1, 1, 3, 2, 1, 1, 2, 1, 1, 1, 1, 1] = 
+# 3*2*2 = 12
+# 567, 57, 67, 7
+#
+# 2: skip zero or 1st (or 2nd if next allows it?)
+# 3: skip zero (skip zero or first), 1st or first 2
+#
+# 3-1 (diff3): 4 options (123, 23, 3, 13, 1, 2)
+# 3-2
+# 3-3: 4 options (1, 2, 3, 123, 23, 3, 13, 1, 2)
+#
+# 3: 7 options: 1,2,3,12,23,13,123
+#    3 alternatives (valid endings): 1, 2,12, 3,23,13,123
+#    all 7, or 6, or 4
+# 2: 3 options: 1,2,12
+#    2 alternatives: 1, 2,12
+def find_recursive(adapters, start_idx=0, chosen_order=None, route_options=None):
+	# Assume sorted
+	# adapters = sorted(adapters)
+
+	if chosen_order is None:
+		chosen_order = []
+	if route_options is None:
+		route_options = []
+
+	# if start_idx != 0:
+	# 	print(start_idx)
+	# 	# return 0
+	curr_idx = start_idx
+	curr_adapter = adapters[start_idx]
+	chosen_order.append(curr_adapter)
+	total_routes = 0
+	while curr_idx <= len(adapters):
+		found, idxs = can_connect(curr_adapter, adapters)
+		# print(curr_idx, found, idxs)
+		if len(idxs) == 0:
+			break
+		route_options.append(len(idxs))
+		# curr_adapter = min(found)
+		# curr_idx = adapters.index(curr_adapter)
+
+		# print(curr_adapter, curr_idx)
+		if len(idxs) == 1:
+			curr_idx = idxs[0]
+			curr_adapter = adapters[curr_idx]
+			chosen_order.append(curr_adapter)
+		else:
+			# Take first route
+			curr_idx = idxs[0]
+			curr_adapter = adapters[curr_idx]
+			# Test alternatives
+			for idx in idxs[1:]:
+				total_routes += find_recursive(adapters, idx, list(chosen_order), list(route_options))
+			chosen_order.append(curr_adapter)
+	# print(start_idx, chosen_order, '\n ', route_options)
+	# print(start_idx, chosen_order)
+	return 1 + total_routes
+
+
+def possibilities(variant):
+	# 13, 31, 1111, 1113
+	# 13, 113, 111_, 21_, 12_
+	# 111/abc -> abc, bc, c
+	# 1111/abc -> abc, bc, c,
+	assert sum(variant) > 3, sum(variant)
+	if variant == [1, 1, 3]:
+		return 2
+	if variant == [1, 1, 1, 3]:
+		return 4
+	if variant == [1, 1, 1, 1]:
+		return 6
+	return 1
+
+
+# Part2 @ 9:15... ?
+if __name__ == '__main__':
+	lines = [line.strip() for line in fileinput.input('input.txt')]
+	test1 = """16
+10
+15
+5
+1
+11
+7
+19
+6
+12
+4"""
+	test2 = """28
+33
+18
+42
+31
+14
+46
+20
+48
+47
+24
+23
+49
+45
+19
+38
+39
+11
+1
+32
+25
+35
+8
+17
+7
+9
+4
+2
+34
+10
+3"""
+	# lines = test2.split('\n')
+	print('Lines: {}'.format(len(lines)))
+	lines = sorted([int(l) for l in lines])
+
+	extra_adapter = 3 + max(lines)
+	lines.append(extra_adapter)
+	# print(lines)
+
+	# PART 2Y - brute force smalls
+	for i in range(2, 10):
+		whole = list(range(1,i))
+		# print(whole)
+		print(len(whole), '=', find_recursive(whole))
+	# exit()
+
+	# Part 2X - diffs
+	# 13 -> 13
+	# 31 -> 3???
+	# 21 -> 21, _1
+	# 11 -> 
+	# 
+
+	diffs = []
+	running = [0]
+	lines = [0] + lines
+	print(lines)
+	for idx in range(len(lines)-1):
+		diff = lines[idx+1] - lines[idx]
+		diffs.append(diff)
+		running.append(running[-1] + diff)
+	print(diffs)
+
+	total = 0
+	total_vals = []
+	poss = []
+	idx = 0
+	while idx < len(diffs):
+		curr = diffs[idx]
+		total += curr
+		total_vals.append(curr)
+		if total > 3:
+			print(total_vals, possibilities(total_vals))
+			poss.append(possibilities(total_vals))
+			total_vals = []
+			total = 0
+			idx -= 1
+		idx += 1
+		# print(curr, end=' ')
+	print()
+	# print(poss)
+	# print(prod(poss))
+	# exit()
+
+	seq_of_ones = 0
+	ones = []
+	for diff in diffs:
+		if diff == 1:
+			seq_of_ones += 1
+		else:
+			ones.append(seq_of_ones)
+			seq_of_ones = 0
+	print(ones)
+	# print(1 + sum(ones))
+	# print(1 + sum([l for l in ones]))
+	vals = [l if l <= 2 else find_recursive(list(range(1,l+1+1))) for l in ones]
+	print(prod([v for v in vals  if v != 0]))
+	exit()
+
+	# # Part 2
+	# lines = [0] + lines
+	# # print(lines)
+	# lines = sorted(lines)
+	# route_count = find_recursive(lines)
+	# print(route_count)
+	# exit()
+
+	# curr_adapter = 0
+	# lines = np.array(lines)
+	# print(lines)
+	# # print(lines[lines > 3])
+	# while curr_adapter < max(lines):
+	# 	print(curr_adapter, curr_adapter - lines)
+	# 	found = can_connect(curr_adapter, lines)
+
+	# 	exit()
+
+	# Part 1
+	chosen_order, diffs, options = find_arrangement(lines)
+
+	print(chosen_order)
+	print(diffs)
+	ones = len([l for l in diffs if l == 1])
+	threes = len([l for l in diffs if l == 3])
+	print(ones, threes, '=', ones * threes)
+
+	print(options)
+	routes = [l if l != 3 else 4 for l in options if l > 1]
+	print(routes)
+	print(sum(routes))
+	print(1 + sum([l for l in options if l > 1]))
+	print(prod([l for l in options if l > 1]))
