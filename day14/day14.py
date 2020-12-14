7ef95ac196a8ec81cf141d235c232e553d6ac2cf
Simon Muller <samullers@gmail.com>
2020-12-14 20:27:56 +0200

    Add functions for converting between int and binary

--- day14/day14.py
+++ day14/day14.py
@@ -1,19 +1,12 @@
 #!/usr/bin/env python3
 import fileinput
-import sys; sys.path.append("..")
-# from lib import *
+
 
 def parse_mask(mask_line):
 	mask_line = mask_line.replace('mask = ', '')
-	mask_dec = int(mask_line.replace('X', '0'), 2)
-	# print()
-	# '{0:b}'.format(37)
-
-	# 
-	# print(masks)
-	# print(mask, commands)
-	# print(mask_line)
+	mask_dec = bin_to_int(mask_line.replace('X', '0'))
 	return mask_line, mask_dec
+
 
 def bin_op(val_str, mask_str):
 	val_str = val_str.zfill(36)
@@ -24,7 +17,7 @@ def bin_op(val_str, mask_str):
 		val_str[idx] = value
 	val_str = ''.join(val_str)
 	# print(val_str)
-	return int(val_str, 2)
+	return bin_to_int(val_str)
 
 
 def mem_loc_op(mem_val, mask_str):
@@ -37,7 +30,7 @@ def mem_loc_op(mem_val, mask_str):
 	mem_val = ''.join(mem_val)
 	x_masks = [idx for idx, m in enumerate(mask_str) if m == 'X']
 	num_of_mems = 2**len(x_masks)
-	variations = ['{0:b}'.format(l).zfill(len(x_masks)) for l in range(num_of_mems)]
+	variations = [int_to_bin(l).zfill(len(x_masks)) for l in range(num_of_mems)]
 	new_mem_locs = []
 	for var in variations:
 		new_mem_loc = list(mem_val)
@@ -45,8 +38,16 @@ def mem_loc_op(mem_val, mask_str):
 		for i, x_pos in enumerate(x_masks):
 			new_mem_loc[x_pos] = var[i]
 		new_mem_loc = ''.join(new_mem_loc)
-		new_mem_locs.append(int(new_mem_loc, 2))
+		new_mem_locs.append(bin_to_int(new_mem_loc))
 	return new_mem_locs
+
+
+def bin_to_int(bin_str):
+	return int(bin_str, 2)
+
+
+def int_to_bin(int_val):
+	return '{0:b}'.format(int_val)
 
 
 # 7:17, 
@@ -67,10 +68,10 @@ if __name__ == '__main__':
 		mem_val = int(mem_val.strip())
 		# print(mem_loc, mem_val)
 		# Part 1
-		p1_memory[mem_loc] = bin_op('{0:b}'.format(mem_val), mask_line)
+		p1_memory[mem_loc] = bin_op(int_to_bin(mem_val), mask_line)
 		# p1_memory[mem_loc] = mem_val | mask_dec
 		# Part 2
-		new_mem_locs = mem_loc_op('{0:b}'.format(int(mem_loc)), mask_line)
+		new_mem_locs = mem_loc_op(int_to_bin(int(mem_loc)), mask_line)
 		# # print(new_mem_locs)
 		for new_mem_loc in new_mem_locs:
 			p2_memory[new_mem_loc] = mem_val
