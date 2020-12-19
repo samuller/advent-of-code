c4d60d9bf990eb111fa622021e82973ac7bbde2c
Simon Muller <samullers@gmail.com>
2020-12-19 09:59:36 +0200

    Day 19 part 1

--- day19/day19.py
+++ day19/day19.py
@@ -5,11 +5,43 @@ from lib import *
 import re
 
 
-def matches_spec(spec, input):
-	possible_values = spec.split('|')
-	if input in possible_values:
-		return True
-	return False
+
+def matches_rec(specs, input, root_spec): #spec_key='0'):
+	# print(input, root_spec)
+	# root_spec = specs[spec_key]
+	# Base case
+	if root_spec.startswith('"'):
+		spec = root_spec[1:-1]
+		if input.startswith(spec):
+			return 1
+		else:
+			return 0
+
+	longest_match = 0
+	paths = root_spec.split(' | ')
+	assert len(paths) <= 2, paths
+	for path in paths:
+		symbols = path.split(' ')
+		if len(symbols) == 1:
+			chars_matching = matches_rec(specs, input, specs[symbols[0]])
+			longest_match = max(longest_match, chars_matching)
+			continue
+		assert len(symbols) == 2, symbols
+		sym1, sym2 = symbols
+
+		n = matches_rec(specs, input, specs[sym1])
+		chars_matching = n
+		if n == 0:
+			continue
+		# Else continue to symbol 2
+		n2 = matches_rec(specs, input[n:], specs[sym2])
+		chars_matching += n2
+		# if chars_matching == len(input):
+		# 	return chars_matching
+
+		longest_match = max(longest_match, chars_matching)
+
+	return longest_match
 
 
 def find_literal(specs):
@@ -32,7 +64,6 @@ def replace_literals(specs):
 
 
 def replace_rules(specs):
-	print(sorted(specs.keys(), key=int, reverse=True))
 	for key in sorted(specs.keys(), key=int, reverse=True):
 		for num in re.findall('\d+', specs[key]):
 			ref_spec = specs[num]
@@ -46,7 +77,9 @@ def replace_rules(specs):
 	return specs
 
 
-# Part 1 - uh... hit my 32Gbs or RAM trying to pre-compute all possibilities...
+# Part 1 - assumed ordered spec
+#  uh... hit my 32Gbs or RAM trying to pre-compute all possibilities...
+#  handled 1-symbol case incorrectly after quickly adding it
 if __name__ == '__main__':
 	lines = [line.strip() for line in fileinput.input()]
 	print('Lines: {}'.format(len(lines)))
@@ -55,23 +88,21 @@ if __name__ == '__main__':
 	raw_spec = next(sections)
 	input = next(sections)
 
-	print(raw_spec)
 	specs = {}
 	for idx, spc in enumerate(raw_spec):
 		id, val = spc.split(': ')
 		specs[id] = val
-		# assert int(id) == idx, id
-		# specs.append(val)
-	print(specs)
-	specs = replace_literals(specs)
-	print(specs)
-	specs = replace_rules(specs)
-	print(specs)
-	final_spec = specs['0'].replace(' ','')
-	print(final_spec)
+	# print(specs)
+
 
 	matches = 0
-	for line in input:
-		if matches_spec(final_spec, line):
+	for idx, line in enumerate(input):
+		# if matches_spec(final_spec, line):
+		# if matches_spec(specs, line):
+		match_len = matches_rec(specs, line, specs['0'])
+		# print('---', match_len,'/',len(line), line)
+		if match_len == len(line):
 			matches += 1
+		# if idx == 2:
+		# 	exit()
 	print(matches)
