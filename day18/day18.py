b026669e9bd366bd25827a6c6564872c6e5ec8e3
Simon Muller <samullers@gmail.com>
2020-12-19 06:56:46 +0200

    Properly combine parts 1 and 2

--- day18/day18.py
+++ day18/day18.py
@@ -8,12 +8,15 @@ def calc_op(op_str):
 	return str(eval(op_str))
 
 
-def calc_str(op_str):
-	# Enable for part 2
+def calc_boam(op_str):
+	# Part 2 - brackets-of-addition-multiplication (not BODMAS)
 	op_str = subrec(r'\d+ \+ \d+', lambda m: calc_op(m.group(0)), op_str)
 	op_str = subrec(r'\d+ \* \d+', lambda m: calc_op(m.group(0)), op_str)
-	# return str(eval(op_str))
-	# Part 1
+	return str(eval(op_str))
+
+
+def calc_bo(op_str):
+	# Part 1 - brackets-only (not BODMAS)
 	# We count=1 so that we successively replace the first value from the left
 	# (otherwise all non-overlapping matches are handled simultaneously)
 	op_str = subrec(r'\d+ [*+] \d+', lambda m: calc_op(m.group(0)), op_str, count=1)
@@ -28,14 +31,18 @@ def subrec(pattern, repl, string, count=0):
 	return string
 
 
-def parse_and_calc(input):
+def parse_and_calc(input, parse_func=calc_bo):
 	# Recursively process parentheses
-	input = subrec(r'\(([^()]+)\)', lambda m: calc_str(m.group(1)), input)
-
-	# print(input)
-	# input = parse_and_replace(r'\d+ [+*\-] \d+', input, False)
-	input = calc_str(input)
+	input = subrec(r'\(([^()]+)\)', lambda m: parse_func(m.group(1)), input)
+	input = parse_func(input)
 	return int(input)
+
+
+def sum_of_calcs(lines, parse_func=calc_bo):
+	sum_ = 0
+	for line in lines:
+		sum_ += parse_and_calc(line, parse_func)
+	return sum_
 
 
 if __name__ == '__main__':
@@ -49,15 +56,15 @@ if __name__ == '__main__':
 	# assert parse_and_calc('5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))') == 12240
 	# assert parse_and_calc('((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2') == 13632
 
-	# assert parse_and_calc('1 + 2 * 3 + 4 * 5 + 6') == 231
-	# assert parse_and_calc('1 + (2 * 3) + (4 * (5 + 6))') == 51
-	# assert parse_and_calc('2 * 3 + (4 * 5)') == 46
-	# assert parse_and_calc('5 + (8 * 3 + 9 + 3 * 4 * 3)') == 1445
-	# assert parse_and_calc('5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))') == 669060
-	# assert parse_and_calc('((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2') == 23340
+	# def parse_and_calc2(inp):
+	# 	return parse_and_calc(inp, parse_func=calc_boam)
+	# assert parse_and_calc2('1 + 2 * 3 + 4 * 5 + 6') == 231
+	# assert parse_and_calc2('1 + (2 * 3) + (4 * (5 + 6))') == 51
+	# assert parse_and_calc2('2 * 3 + (4 * 5)') == 46
+	# assert parse_and_calc2('5 + (8 * 3 + 9 + 3 * 4 * 3)') == 1445
+	# assert parse_and_calc2('5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))') == 669060
+	# assert parse_and_calc2('((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2') == 23340
 	# exit()
 	
-	sum_ = 0
-	for line in lines:
-		sum_ += parse_and_calc(line)
-	print(sum_)
+	print(sum_of_calcs(lines, parse_func=calc_bo))
+	print(sum_of_calcs(lines, parse_func=calc_boam))
