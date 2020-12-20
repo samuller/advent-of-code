d62ab27a56659e0e18fa8c238772071df80be775
Simon Muller <samullers@gmail.com>
2020-12-20 10:15:47 +0200

    Day 20 part 2 without rotations and flips

--- day20/day20.py
+++ day20/day20.py
@@ -4,6 +4,7 @@ import sys; sys.path.append("..")
 from lib import *
 from collections import defaultdict
 from math import sqrt
+import itertools
 
 
 def print_tile(tile_data):
@@ -48,7 +49,6 @@ def check_match(tiles, tile_key1, tile_key2):
 	# All sides
 	tblr1 = [top_line(tile1), bottom_line(tile1), left_line(tile1), right_line(tile1)]
 	tblr2 = [top_line(tile2), bottom_line(tile2), left_line(tile2), right_line(tile2)]
-	side_key = ['top', 'bottom', 'left', 'right']
 
 	# print_tile(tile1)
 	match = match_sides(0, tblr1, tblr2)
@@ -63,10 +63,6 @@ def check_match(tiles, tile_key1, tile_key2):
 	match = match_sides(3, tblr1, tblr2)
 	if match:
 		return match
-	# print('top', top_line)
-	# print('bottom', bottom_line)
-	# print('left', left_line)
-	# print('right', right_line)
 	return None
 
 
@@ -75,21 +71,35 @@ def get_neighbours(pairings):
 	for pair, match in pairings.items():
 		tile_key1, tile_key2 = pair
 
-		tile_types[tile_key1].append(match)
-		tile_types[tile_key2].append(match)
+		tile_types[tile_key1].append(tile_key2)
+		tile_types[tile_key2].append(tile_key1)
+		# tile_types[tile_key1].append(match)
+		# tile_types[tile_key2].append(match)
 	return tile_types
 
 
-def get_corners(pairings):
+def get_sides_with_count(pairings, count=2):
 	corners = []
 	tile_types = get_neighbours(pairings)
 
 	for key in tile_types.keys():
 		# print(key, tile_types[key])
-		if len(tile_types[key]) == 2:
+		if len(tile_types[key]) == count:
 			corners.append(key)
 
 	return corners
+
+
+def get_corners(pairings):
+	return get_sides_with_count(pairings, 2)
+
+
+def get_sides(pairings):
+	return get_sides_with_count(pairings, 3)
+
+
+def get_insides(pairings):
+	return get_sides_with_count(pairings, 4)
 
 
 def remove_borders(tile_data):
@@ -110,7 +120,7 @@ def find_pairings(tiles):
 			# print(tile_key1, tile_key2)
 			match = check_match(tiles, tile_key1, tile_key2)
 			if match:
-				print(tile_key1, tile_key2, match)
+				# print(tile_key1, tile_key2, match)
 				pairings[(tile_key1, tile_key2)] = match
 	return pairings
 
@@ -119,52 +129,166 @@ def rotate(tile_data, times):
 	pass
 
 
+def count_values(tile_data, value='#'):
+	count = 0
+	for line in tile_data:
+		count += line.count(value)
+	return count
+
+
+def count_all_values(tiles, value='#'):
+	total_count = 0
+	for name, tile_data in tiles.items():
+		total_count += count_values(tile_data, value)
+	return total_count
+
+
+def count_missing_tiles(tiling):
+	count_nones = 0
+	for row in tiling:
+		count_nones += row.count(None)
+	return count_nones
+
+
+def empty_tiling(side_len):
+	return [[None for _ in range(side_len)] for _ in range(side_len)]
+
+
+def print_dict(dic):
+	for key in sorted(dic.keys()):
+		print(key, dic[key])
+	print()
+
+
+def get_orientation(pairings, tile, friend, friend_pos):
+	rotate = 0
+	flip = 0
+	for key, match in pairings.items():
+		t1, t2 = key
+		if (tile, friend) == key:
+			print(tile, friend, match)
+		elif (friend, tile) == key:
+			print(tile, friend, match)
+	# print(pairings)
+	return (rotate, flip)
+
+
 def get_full_tiling(tiles, pairings):
 	"""Basically solving a puzzle"""
 	side_len = sqrt(len(tiles))
 	assert side_len == int(side_len)
 	side_len = int(side_len)
-
-	tiling = tiles.keys()
-	tiling = [[None for _ in range(side_len)] for _ in range(side_len)]
-
 	# Possible pairings for square (horizontal + vertical)
 	pairs = 2*side_len*(side_len-1)
 	assert pairs == len(pairings)
 	print('Pairs:', pairs, '({})'.format(side_len))
 
+	tiling = empty_tiling(side_len)
+
+	neighbours = get_neighbours(pairings)
+	# print_dict(neighbours)
 	corners = get_corners(pairings)
-	print('Corners:', corners)
+	name = corners[0]
+	tiling[0][0] = corners[0]
+	tile_orientation = {}
+	iterations = 0
+	while count_missing_tiles(tiling) != 0:
+		for r,c in itertools.product(range(side_len), range(side_len)):
+			cell = tiling[r][c]
+			if cell is None:
+				continue
+			friends = list(neighbours[cell])
+			for dr,dc in [[-1, 0], [1, 0], [0, -1], [0, 1]]:
+				if not 0 <= r+dr < side_len or not 0 <= c+dc < side_len:
+					continue
+				if tiling[r+dr][c+dc] is None:
+					friend = friends.pop()
+					tile_orientation[friend] = get_orientation(pairings, cell, friend, (dr,dc))
+					tiling[r+dr][c+dc] = friend
+				if len(friends) == 0:
+					break
+		iterations += 1
+		# print(tiling)
+		# if iterations == 2:
+		# 	exit()
 
-	print(pairings)
-	tile_types = get_neighbours(pairings)
-	for key in tile_types:
-		print(key, tile_types[key])
-	print()
-
-	print('Tiling:', tiling)
-	return tiling
+	print('Tiling:')
+	for line in tiling:
+		print('   '.join([str(l) for l in line]))
+	return tiling, tile_orientation
 
 
 def build_full_image(tiles, pairings):
+	tiling, tile_orientation = get_full_tiling(tiles, pairings)
+	# assert tiling == [
+	# 	[1951, 2311, 3079],
+	# 	[2729, 1427, 2473],
+	# 	[2971, 1489, 1171]]
+	side_len = int(sqrt(len(tiles)))
+	tile_len = None
+	for name, tile_data in tiles.items():
+		if tile_len is not None:
+			assert tile_len == len(tile_data[0]), '{} != {}'.format(
+				tile_len, len(tile_data[0]))
+		tile_len = len(tile_data[0])
+	print('{}x{}'.format(tile_len, tile_len))
 
-	return tiles[3079]
+	# full_tiles = [[tiles[tiling[r][c]] for r in range(side_len)] for c in range(side_len)]
+	image = []
+	for r in range(side_len):
+		for _ in range(tile_len):
+			image.append('')
+		for c in range(side_len):
+			name = tiling[r][c]
+			tile_data = tiles[name]
+			# TODO: rotations & flips
+			print(name, tile_orientation[name])
+			for idx, tr in enumerate(tile_data):
+				image[r*tile_len + idx] = image[r*tile_len + idx] + tr
+			# print(tile_data)
+			# print_tile(tile_data)
+			# exit()
+
+	assert len(image) == side_len * tile_len
+	for line in image:
+		assert len(line) == side_len * tile_len
+
+	return image
+
+
+def match_template(image, template, im_row, im_col):
+	for tmp_row in range(len(template)):
+		for tmp_col in range(len(template[tmp_row])):
+			tmp_val = template[tmp_row][tmp_col]
+			# If no space for matching rest of template
+			if im_row+tmp_row >= len(image) or \
+				im_col+tmp_col >= len(image[im_row+tmp_row]):
+				return False
+			im_val = image[im_row+tmp_row][im_col+tmp_col]
+			if tmp_val  == '#' and im_val != '#':
+				return False
+	return True
 
 
 def find_pattern(tiles, pairings, pattern):
 	assert len(pattern) == 3, len(pattern)
 	assert len(pattern[0]) == 20, len(pattern[0])
+	assert match_template(pattern, pattern, 0, 0)
 
 	image = build_full_image(tiles, pairings)
 	print_tile(image)
-	return None
+	print_tile(pattern)
+	dim = len(image)
+	# Template matching
+	matches = []
+	for im_row in range(dim):
+		for im_col in range(dim):
+			if match_template(image, pattern, im_row, im_col):
+				matches.append((im_row, im_col))
+	return matches
 
-	# for name, tile in tiles.items():
-	# 	print(len(tile))
-	# 	return
-	# return 0
 
-
+# 2418 (guess of 2) @ 8:37
 if __name__ == '__main__':
 	lines = [line.strip() for line in fileinput.input()]
 	print('Lines: {}'.format(len(lines)))
@@ -194,12 +318,6 @@ if __name__ == '__main__':
 	print(prod(corners))
 
 	# Part 2
-	tiling = get_full_tiling(tiles, pairings)
-	assert tiling == [
-		[1951, 2311, 3079],
-		[2729, 1427, 2473],
-		[2971, 1489, 1171]]
-
 
 	# Remove borders
 	for name in tiles.keys():
@@ -213,4 +331,10 @@ if __name__ == '__main__':
 	['                  # ',
 	 '#    ##    ##    ###',
 	 ' #  #  #  #  #  #   ']
-	print(find_pattern(tiles, pairings, sea_monster))
+	patterns_found = find_pattern(tiles, pairings, sea_monster)
+	print('Patterns:', patterns_found)
+	print(len(patterns_found))
+
+	total_count = count_all_values(tiles)
+	print('Total count', total_count)
+	print(total_count - 30*len(patterns_found))
