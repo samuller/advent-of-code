9cc71008055d567453ab5041ac557e6118ffd1d0
Simon Muller <samullers@gmail.com>
2020-12-25 07:55:28 +0200

    Remove unused code

--- day25/day25.py
+++ day25/day25.py
@@ -1,38 +1,7 @@
 #!/usr/bin/env python3
 import fileinput
-# import sys; sys.path.append("..")
-# from lib import *
 from collections import defaultdict
 
-
-class Classy:
-	def __init__(self):
-		pass
-
-
-memoize = defaultdict(dict)
-def transform_memoize(subject_num, loop_size):
-	start_loop = 0
-	value = 1
-
-	if subject_num in memoize:
-		if loop_size in memoize[subject_num]:
-			return memoize[subject_num][loop_size]
-		else:
-			start_loop = max(memoize[subject_num].keys())
-			if start_loop < loop_size:
-				value = memoize[subject_num][start_loop]
-			else:
-				start_loop = 0
-
-	# print(memoize[7])
-	for _ in range(start_loop, loop_size):
-		value = value * subject_num
-		value = value % 20201227
-
-	memoize[subject_num][loop_size] = value
-	# print(memoize[7])
-	return value
 
 def transform(subject_num, loop_size, start_loop=0, start_value=1):
 	value = start_value
@@ -46,11 +15,8 @@ def find_transform(subject_num, result_num):
 	trans = 1
 	loop = 1
 	while True:
-	# for loop in range(1, 1_000_000):
 		if loop % 10_000_000 == 0:
-			print(loop)
-		# 	print(loop, len(memoize[subject_num]) if subject_num in memoize else 0)
-		# trans = transform_memoize(subject_num, loop)
+			print('Progress... {}'.format(loop))
 		trans = transform(subject_num, loop, start_loop=loop-1, start_value=trans)
 		if trans == result_num:
 			return loop
@@ -59,8 +25,7 @@ def find_transform(subject_num, result_num):
 
 
 def main():
-	# lines = [line.strip() for line in fileinput.input()]
-	# print('Lines: {}'.format(len(lines)))
+	lines = [line.strip() for line in fileinput.input()]
 
 	# card_pub = transform(7)
 	# door_pub = transform(7)
@@ -78,8 +43,8 @@ def main():
 	# assert transform(5764801, 11) == 14897079
 	# exit()
 
-	card_pub = 15628416
-	door_pub = 11161639
+	card_pub = int(lines[0]) # 15628416
+	door_pub = int(lines[1]) # 11161639
 
 	card_loops = find_transform(7, card_pub)
 	door_loops = find_transform(7, door_pub)
