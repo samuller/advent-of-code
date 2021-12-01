39c9a373cdd3d3441605a03af34704eb439b5b42
Simon Muller <samullers@gmail.com>
2021-12-01 07:43:54 +0200

    Clean-up and try shorter approach
    Not necessarily a better approach.

--- 2021/day1/day1.py
+++ 2021/day1/day1.py
@@ -1,36 +1,27 @@
 #!/usr/bin/env python3
 import fileinput
-import itertools
-import sys; sys.path.append("../..")
-from lib import *
+from collections import Counter
 
 
 # bugs:
 # 1509 - off by one error on indices
 # 1998 - same, on 2nd prev + if offset
 def main():
-    lines = [int(line.strip()) for line in fileinput.input()]
-    print('Lines: {}'.format(len(lines)))
+    nums = [int(line.strip()) for line in fileinput.input()]
+    print('Lines: {}'.format(len(nums)))
 
     # Part 1
-    count = 0
-    prev = lines[0]
-    for num in lines:
-        if num > prev:
-            count += 1
-        prev = num
+    nums1 = nums[:-1]
+    nums2 = nums[1:]
+    assert len(nums1) == len(nums2)
+    # Previous approach with for-loop is probably more adaptable
+    larger = [nums2[i] > nums1[i] for i in range(len(nums1))]
+    count = Counter(larger)[True]
     print(count)
 
     # Part 2
-    count = 0
-    prev = lines[0:3]
-    for idx, num in enumerate(lines):
-        if idx > len(lines) - 3:
-            continue
-        curr = lines[idx:idx+3]
-        if sum(curr) > sum(prev):
-            count += 1
-        prev = lines[idx:idx+3]
+    larger = [sum(nums[i+1:i+1+3]) > sum(nums[i:i+3]) for i in range(len(nums1)-2)]
+    count = Counter(larger)[True]
     print(count)
 
 
