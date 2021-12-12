8f5b724d742d77915c9b0b7c509049b5dc18c7f8
Simon Muller <samullers@gmail.com>
2021-12-12 07:55:36 +0200

    Actively combine part 1 and part 2

--- 2021/day12/day12.py
+++ 2021/day12/day12.py
@@ -14,15 +14,21 @@ assert is_small_cave('abc') == True
 assert is_small_cave('aBc') == False
 
 
-def is_valid_path(current_path):
+def part1_is_valid_path(current_path):
+    counts = Counter(current_path)
+    for node, count in counts.items():
+        if is_small_cave(node) and count > 1:
+            return False
+        if node in ['start', 'end'] and count > 1:
+            return False
+    return True
+
+
+def part2_is_valid_path(current_path):
     counts = Counter(current_path)
     duplicate_exists = False
     for node, count in counts.items():
         if is_small_cave(node):
-            # Part 1
-            # if count > 1:
-            #     return False
-            # Part 2
             if count > 1 and duplicate_exists:
                 return False
             if count > 2:
@@ -34,19 +40,18 @@ def is_valid_path(current_path):
 
     return True
 
-# Part 2
-assert is_valid_path(['start', 'A', 'c', 'A', 'c']) == True
-assert is_valid_path(['start', 'A', 'c', 'A', 'c', 'A', 'c']) == False
+assert part2_is_valid_path(['start', 'A', 'c', 'A', 'c']) == True
+assert part2_is_valid_path(['start', 'A', 'c', 'A', 'c', 'A', 'c']) == False
 
 
 all_paths = []
-def unique_paths(network, current_node='start', current_path=None):
+def unique_paths(network, current_node='start', current_path=None, validation_func=part2_is_valid_path):
     if current_path is None:
         current_path = []
     current_path.append(current_node)
     # print(current_path)
 
-    if not is_valid_path(current_path):
+    if not validation_func(current_path):
         return None
 
     if current_node == 'end':
@@ -56,52 +61,31 @@ def unique_paths(network, current_node='start', current_path=None):
 
     next_nodes = network[current_node]
     for node in next_nodes:
-        # if is_small_cave(node):
-        #     if node in current_path:
-        #         continue
-        unique_paths(network, node, copy(current_path))
+        unique_paths(network, node, copy(current_path), validation_func)
 
     return None
-
-# Original part1
-# def unique_paths(network, current_node='start', current_path=None):
-#     if current_path is None:
-#         current_path = []
-#     current_path.append(current_node)
-
-#     if current_node == 'end':
-#         all_paths.append(current_path)
-#         return current_path
-
-#     next_nodes = network[current_node]
-#     for node in next_nodes:
-#         if is_small_cave(node):
-#             if node in current_path:
-#                 continue
-#         unique_paths(network, node, copy(current_path))
-
-#     return current_path
 
 
 def main():
     lines = [line.strip() for line in fileinput.input()]
-    print(f'Lines: {len(lines)}')
 
     nodes = set()
-    network = defaultdict(list)
+    next_nodes = defaultdict(list)
     for line in lines:
         left, right = line.split('-')
         nodes.add(left)
         nodes.add(right)
-        network[left].append(right)
-        network[right].append(left)
+        next_nodes[left].append(right)
+        next_nodes[right].append(left)
     assert 'start' in nodes
     assert 'end' in nodes
-    print(nodes)
-    print(network)
 
-    unique_paths(network, 'start', [])
-    print(len(all_paths))
+    global all_paths
+    unique_paths(next_nodes, 'start', [], validation_func=part1_is_valid_path)
+    print('P1:', len(all_paths))
+    all_paths = []
+    unique_paths(next_nodes, 'start', [], validation_func=part2_is_valid_path)
+    print('P2:', len(all_paths))
     
     # Part 1
     # assert unique_paths(test1) == 10
