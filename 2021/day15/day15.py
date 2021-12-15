01ee8fb657c5079e213f0f243e91ccd60edff5f2
Simon Muller <samullers@gmail.com>
2021-12-15 17:19:31 +0200

    Day 15 part 2

--- 2021/day15/day15.py
+++ 2021/day15/day15.py
@@ -11,9 +11,20 @@ EndLoc = namedtuple('EndLoc', ['len', 'cost', 'pos'])
 
 
 def heuristic(map, pos, goal_pos):
+    r,c = pos
+    val = map[r][c]
     dr = goal_pos[0] - pos[0]
     dc = goal_pos[1] - pos[1]
-    return math.sqrt(dr**2 + dc**2)
+    dist = math.sqrt(dr**2 + dc**2)
+    return val + dist
+
+
+def reconstruct_path(came_from, pos):
+    path = [pos]
+    while pos in came_from:
+        pos = came_from[pos]
+        path.append(pos)
+    return list(reversed(path))
 
 
 def a_star(map, start_pos=(0,0), goal_pos=None):
@@ -38,10 +49,14 @@ def a_star(map, start_pos=(0,0), goal_pos=None):
         open_pos_set.remove(curr.pos)
         # print(curr_f_score, curr)
         if curr.pos == goal_pos:
+            # print(came_from[end])
+            path = reconstruct_path(came_from, goal_pos)
+            # print(path)
+            print(sum([map[p[0]][p[1]] for p in path]) - map[path[0][0]][path[0][1]])
             return curr
 
         r, c = curr.pos
-        for dr, dc in [(0,1),(1,0)]:
+        for dr, dc in [(0,1),(1,0),(0,-1),(-1,0)]:
             rr, cc = r + dr, c + dc
             if not (0<=rr<R and 0<=cc<C):
                 continue
@@ -60,6 +75,24 @@ def a_star(map, start_pos=(0,0), goal_pos=None):
     assert False
 
 
+def expand_map(map, times=5):
+    R = len(map)
+    C = len(map[0])
+    # big_map = [[0]*C*5]*R*5
+    big_map = [[0 for _ in range(C*5)] for _ in range(R*5)]
+    for r, row in enumerate(big_map):
+        for c, val in enumerate(row):
+            # print(r,c)
+            val = map[r % R][c % C]
+            add = (r // R) + (c // C)
+            val = 1 + ((val - 1 + add) % 9)
+            big_map[r][c] = val
+
+            if 0<=r<R and 0<=c<C:
+                assert big_map[r][c] == map[r][c]
+    return big_map
+
+
 def print_map(map):
     for r, row in enumerate(map):
         for c, val in enumerate(row):
@@ -67,6 +100,10 @@ def print_map(map):
         print()
     print()
 
+# 14:28 - 2973 (too high) - no up or left movements
+# 14:37 - 2970 (too high) - heuristic not good enough when including left & up?
+# 14:50 - 2969 (too high) - guess
+# 15:02 - cost somehow took starting value into account again
 def main():
     lines = [line.strip() for line in fileinput.input()]
     print(f'Lines: {len(lines)}')
@@ -77,8 +114,23 @@ def main():
     R = len(map)
     C = len(map[0])
 
+    # # Test equation for height changes
+    # for r in range(5):
+    #     for c in range(5):
+    #         val = 8
+    #         add = r + c
+    #         val = 1 + ((val - 1 + add) % 9)
+    #         # val += 1
+    #         print(val, end=" ")
+    #         # print((r,c), end=" ")
+    #     print()
+    # print()
 
     print(a_star(map))
+
+    big_map = expand_map(map)
+    # print_map(big_map)
+    print(a_star(big_map))
 
 
 if __name__ == '__main__':
