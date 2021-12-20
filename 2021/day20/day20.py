25017451bc37da97ca0da487ccfcbb4c200c7c91
Simon Muller <samullers@gmail.com>
2021-12-20 16:17:47 +0200

    Clean-up Day 20

--- 2021/day20/day20.py
+++ 2021/day20/day20.py
@@ -18,16 +18,18 @@ def print_img(pixels):
     print()
 
 
+def print_dims(pixels):
+    rs = [r for r,_ in pixels]
+    cs = [c for _,c in pixels]
+    print(f"{max(rs)-min(rs)}x{max(cs)-min(cs)}")
+
+
 def enhance(pixels, algo, swap=False):
     rs = [r for r,_ in pixels]
     cs = [c for _,c in pixels]
-    # print(rs)
-    # print(cs)
 
     new_pixels = set()
-    ext = 3
-    # for pixel in pixels:
-    #     r,c = pixel
+    ext = 2
     for r in range(min(rs)-ext, max(rs)+1+ext):
         for c in range(min(cs)-ext, max(cs)+1+ext):
             binary_value = []
@@ -38,7 +40,6 @@ def enhance(pixels, algo, swap=False):
                 else:
                     binary_value.append('0' if not swap else '1')
             value = int("".join(binary_value), 2)
-            # print(r,c,value)
             if algo[value] == '#':
                 new_pixels.add((r,c))
     return new_pixels
@@ -68,13 +69,18 @@ def main():
     lines = [line.strip() for line in fileinput.input()]
     groups = list(grouped(lines))
     algo = groups[0][0]
+    input = groups[1]
+
     assert len(algo) == 512
     assert (algo[0],algo[511]) in [('.','#'), ('#','.')]
-    input = groups[1]
+
+    handle_inversions = False
+    if algo[0] == '#':
+        handle_inversions = True
 
     R = len(input)
     C = len(input[0])
-    print(f'Input: {R}x{C}')
+    # print(f'Input: {R}x{C}')
     pixels = set()
     for r in range(R):
         for c in range(C):
@@ -82,32 +88,18 @@ def main():
             if input[r][c] == '#':
                 pixels.add((r,c))
 
-    print_img(pixels)
-    print(len(pixels))
+    # print_img(pixels)
+    # print(len(pixels))
     for i in range(50):
-        if i % 2 == 0:
+        if i == 2:
+            print(len(pixels))
+        if not handle_inversions or i % 2 == 0:
             pixels = enhance(pixels, algo, False)
         else:
             pixels = inverse(pixels)
             pixels = enhance(pixels, algo, True)
-            # pixels = inverse(pixels)
-
-        rs = [r for r,_ in pixels]
-        cs = [c for _,c in pixels]
-        print(f"{max(rs)-min(rs)}x{max(cs)-min(cs)}")
-        # img_pixels = {(r,c) for r,c in pixels 
-        #     if r != min(rs) and r != max(rs) and c != min(cs) and c != max(cs)}
-        # border_pixels = {(r,c) for r,c in pixels 
-        #     if r == min(rs) or r == max(rs) or c == min(cs) or c == max(cs)}
-        # pixels = img_pixels
-        # print_img(pixels)
-        # if i % 2 == 1:
-        #     print('border ', end="")
-            # 4 + 106*4
-            # 5,7,9 / 100,103,105
-            # print(len(border_pixels))
-        # else:
-        print(len(pixels))
+        # print_dims(pixels)
+    print(len(pixels))
 
 
 if __name__ == '__main__':
