abeced19fd0abfa637b6331ca835e1ccff773e5e
Simon Muller <samullers@gmail.com>
2022-12-01 07:56:03 +0200

    Day 1 - 2022!

---
+++ 2022/day1.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import fileinput
+from collections import Counter
+
+import sys; sys.path.append("..")
+from lib import *
+
+
+def main():
+    lines = [line.strip() for line in fileinput.input()]
+    elves = []
+    for elf in grouped(lines):
+        total = sum([int(cal) for cal in elf])
+        elves.append(total)
+    elves = sorted(elves)
+    print(elves[-1])
+    print(sum(elves[-3:]))
+
+
+if __name__ == '__main__':
+    main()
