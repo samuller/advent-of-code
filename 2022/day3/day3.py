94bcd4456ab38fb2660481fd835e567123de667b
Simon Muller <samullers@gmail.com>
2022-12-03 08:31:44 +0200

    Day 3 clean-up

--- 2022/day3/day3.py
+++ 2022/day3/day3.py
@@ -2,7 +2,7 @@
 import fileinput
 from collections import Counter
 
-import sys; sys.path.append("..")
+import sys; sys.path.append("../..")
 from lib import *
 
 
@@ -13,58 +13,56 @@ def get_priority(val):
         return 27 + ord(val) - ord('A')
     assert False
 
-# print('A', get_priority('A'))
-# print('Z', get_priority('Z'))
-# print('a', get_priority('a'))
-# print('z', get_priority('z'))
+
+assert get_priority('a') == 1
+assert get_priority('z') == 26
+assert get_priority('A') == 27
+assert get_priority('Z') == 52
+
 
 def get_badge(group):
+    assert len(group) == 3
     count_grp = Counter([*set(group[0]), *set(group[1]), *set(group[2])])
-    print(group)
-    print(count_grp)
-    summ2 = 0
-    for val2 in count_grp:
-        if count_grp[val2] == 3:
-            assert summ2 == 0
-            print(val2)
-            summ2 = get_priority(val2)
-    return summ2
+    badges = [item for item, count in count_grp.items() if count == 3]
+    assert len(badges) == 1
+    return get_priority(badges[0])
+
+
+def get_dups(comp1, comp2):
+    count = Counter([*set(comp1), *set(comp2)])
+    dups = [item for item, cnt in count.items() if cnt > 1]
+    assert len(dups) == 1
+    return get_priority(dups[0])
 
 
 # 8:07: 2788 - too low
 def main():
-    lines = [line.strip() for line in fileinput.input()]
-    total1 = 0
-    total2 = 0
-    for idx, bag in enumerate(lines):
-        # print(line)
+    bags = [line.strip() for line in fileinput.input()]
+    part1 = 0
+    part2 = 0
+    for idx, bag in enumerate(bags):
         sz = len(bag)
-        # print(sz, sz//2)
         comp1 = bag[0:sz//2]
         comp2 = bag[sz//2:]
-        # print(comp1, comp2)
-        count = Counter([*set(comp1), *set(comp2)])
-        summ = 0
-        for val in count:
-            if count[val] > 1:
-                assert summ == 0
-                summ = get_priority(val)
-        # print(summ)
-        total1 += summ
-        # Part 2
+        part1 += get_dups(comp1, comp2)
+        # Part 2 looks only at groups of 3
         if idx % 3 == 0 and idx != 0:
-            group = lines[idx-3:idx]
-            total2 += get_badge(group)
+            group = bags[idx-3:idx]
+            part2 += get_badge(group)
+    # Remember last group of bags
     idx += 1
     if idx % 3 == 0 and idx != 0:
-        group = lines[idx-3:idx]
-        total2 += get_badge(group)
-    print(total1)
-    print(total2)
-    assert total1 == 157
-    assert total2 == 70
-    # assert total1 == 8185
-    # assert total2 == 2817
+        group = bags[idx-3:idx]
+        part2 += get_badge(group)
+
+    print(part1)
+    print(part2)
+    # Test
+    # assert total1 == 157
+    # assert total2 == 70
+    # Input
+    # assert part1 == 8185
+    # assert part2 == 2817
 
 
 if __name__ == '__main__':
