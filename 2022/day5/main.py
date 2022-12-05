62824e30829477bae779aa496893604fedc4fd46
Simon Muller <samullers@gmail.com>
2022-12-05 07:48:50 +0200

    Slight cleanup

--- 2022/day5/main.py
+++ 2022/day5/main.py
@@ -6,24 +6,24 @@ import sys; sys.path.append("../..")
 from lib import *
 
 
+# Delays: strip(), wrong ordering of loops, numbers below stacks, last column not being 4 wide
 def main():
+    # Can't strip as white space has meaning of empty stack
     lines = [line.replace("\n", "") for line in fileinput.input()]
     groups = grouped(lines)
-    # for group in groups:
-    #     print(group)
-    # exit()
-    lines_stacks = next(groups)
 
+    lines_stacks = next(groups)
     counts = [int(n) for n in lines_stacks[-1].split()]
     assert counts == list(range(1, 1+counts[-1]))
     # Throw away numbers
     lines_stacks = lines_stacks[0:-1]
+
     stacks = []*counts[-1]
     for i in range(0, len(lines_stacks[0]), 4):
         stack = []
         for line in lines_stacks:   
             crate = line[i:i+4]
-            print(f"_{crate}_[{i}:{i+4}]")
+            # print(f"_{crate}_[{i}:{i+4}]")
             crate = crate.strip()
             if len(crate) == 0:
                 continue
@@ -32,28 +32,34 @@ def main():
         # Put top at the right
         stack.reverse()
         stacks.append(stack)
-    print(stacks)
+    # print(stacks)
 
     line_cmds = next(groups)
-    print(line_cmds)
+    # print(line_cmds)
+    part1_stacks = stacks
+    # Make copy
+    part2_stacks = [list(stack) for stack in part1_stacks]
     for cmd in line_cmds:
         acts = cmd.strip().split()
         count = int(acts[1])
         from_id = int(acts[3]) - 1
         to_id = int(acts[5]) - 1
-        print(cmd)
-        print(count, from_id, to_id)
-        print(stacks)
-        # Part 1 - started at 7:23 (after formatting code)
-        # for _ in range(count):
-        #     crate = stacks[from_id].pop()
-        #     stacks[to_id].append(crate)
+        # print(cmd)
+        # print(count, from_id, to_id)
+        # print(part1_stacks)
+
+        # Part 1 - started at 7:23 (time to write parsing code...)
+        for _ in range(count):
+            crate = part1_stacks[from_id].pop()
+            part1_stacks[to_id].append(crate)
         # Part 2
-        move_stack = stacks[from_id][-count:]
-        stacks[from_id] = stacks[from_id][:-count]
-        stacks[to_id].extend(move_stack)
-    # 7:26 & 7:29
-    print("".join([stack[-1] for stack in stacks]))
+        move_stack = part2_stacks[from_id][-count:]
+        part2_stacks[from_id] = part2_stacks[from_id][:-count]
+        part2_stacks[to_id].extend(move_stack)
+    # 7:26
+    print("".join([stack[-1] for stack in part1_stacks]))
+    # 7:29
+    print("".join([stack[-1] for stack in part2_stacks]))
 
 
 
