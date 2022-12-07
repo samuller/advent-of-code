5115a5b14502b356e10e64163343015d38f294ce
Simon Muller <samullers@gmail.com>
2022-12-07 10:10:41 +0200

    Day 7 part 2

--- 2022/day7/main.py
+++ 2022/day7/main.py
@@ -7,7 +7,7 @@ from lib import *
 
 
 def root_add(contents, path, file=None, size=0):
-    print("ADD", contents, path)
+    # print("ADD", contents, path)
     assert path.startswith("/")
     curr = contents
     if path != "/":
@@ -33,26 +33,30 @@ def process_ls_output(root_contents, curdir, curdir_contents):
             root_add(root_contents, f"{curdir}/{res[1]}".replace("//", "/"))
         else:
             root_add(root_contents, f"{curdir}", file=res[1], size=int(res[0]))
-    print(curdir_contents)
+    # print(curdir_contents)
 
 
 def recurse_size(contents, size_limit=None):
     assert type(contents) == dict
     total = 0
     dirsum = 0
+    dir_sizes = []
     for key in contents:
         curr = contents[key]
         if type(curr) == dict:
-            res, child_dirsum = recurse_size(curr, size_limit)
-            print("SUB", key, res)
+            res, child_dirsum, child_dir_sizes = recurse_size(curr, size_limit)
+            dir_sizes.append((key, res))
+            dir_sizes.extend(child_dir_sizes)
+            # print("SUB", key, res)
             if size_limit is not None:
                 if res > size_limit:
                     # print("SKIP", key, res)
                     # continue
                     pass
                 else:
-                    print("DIRSUM", key, res)
+                    # print("DIRSUM", key, res)
                     dirsum += res
+
             dirsum += child_dirsum
             total += res
             continue
@@ -60,12 +64,17 @@ def recurse_size(contents, size_limit=None):
         size = curr
         assert type(size) == int, type(size)
         total += size
-    return total, dirsum
+    return total, dirsum, dir_sizes
 
 
+# 2 hour power delay 
+# part 2: 19935440 - too hight
 def main():
     lines = [line.replace("\n", "") for line in fileinput.input()]
-    print(lines)
+    # print(lines)
+    total_space = 70_000_000
+    unused_space_needed = 30_000_000
+
     root_contents = {}
     curdir = None
     curdir_contents = []
@@ -98,16 +107,26 @@ def main():
         elif ls_results:
             result = line.split()
             curdir_contents.append(line)
-        print(curdir)
+        # print(curdir)
 
     if ls_results:
         process_ls_output(root_contents, curdir, curdir_contents)
         ls_results = False
         curdir_contents = []
 
-    print(root_contents)
-    print(recurse_size(root_contents))
-    print(recurse_size(root_contents, size_limit=100_000))
+    # print(root_contents)
+    # print(recurse_size(root_contents))
+    total_used, smalldirs, alldirs = recurse_size(root_contents, size_limit=100_000)
+    # print(smalldirs)
+    total_unused = total_space - total_used
+    space_need_to_free = unused_space_needed - total_unused
+    # print(space_need_to_free)
+    # print(alldirs)
+    for dir_size in sorted(alldirs, key=lambda t: t[1]):
+        dir, size = dir_size
+        if size >= space_need_to_free:
+            print(dir, size)
+            break
 
 
 if __name__ == '__main__':
