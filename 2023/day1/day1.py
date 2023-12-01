d8d50a1abfbfb42ad5185035d621b7c0c25dd729
Simon Muller <samullers@gmail.com>
2023-12-01 08:20:56 +0200

    Clean-up/combine

--- 2023/day1/day1.py
+++ 2023/day1/day1.py
@@ -16,20 +16,6 @@ def isnum(input):
 NUMBERS = ["_ZERO_X", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"]
 
 
-def has_num_name(word):
-    for val in NUMBERS:
-        if val in word:
-            return True
-    return False
-
-
-def get_num_name(word):
-    for idx in range(len(word)):
-        if word[idx:] in NUMBERS:
-            return word[idx:]
-    return None
-
-
 def ending_num_name(word):
     for idx, val in enumerate(NUMBERS):
         if word.endswith(val):
@@ -37,52 +23,17 @@ def ending_num_name(word):
     return None
 
 
-# 7:28 54623 [overlapping]
-# 54591
+# 7:28 54623 [overlapping - test with 4threeight7]
 def main():
     lines = [line.strip() for line in fileinput.input()]
-    print(f'Lines: {len(lines)}')
 
-    # Part 1
-    # values = []
-    # for line in lines:
-    #     # fields = line.split(' ')
-    #     line = "".join([a for a in line if isnum(a)])
-    #     line = line[0] + line[-1]
-    #     # print()
-    #     values.append(int(line))
-    # print(sum(values))
-
-    # Part 2 wrong
-    # values = []
-    # for line in lines:
-    #     nums = []
-    #     curr_word = ""
-    #     for char in line:
-    #         if has_num_name(curr_word):
-    #             curr_num = NUMBERS.index(get_num_name(curr_word))
-    #             nums.append(curr_num)
-    #             curr_word = ""
-    #         if isnum(char):
-    #             nums.append(int(char))
-    #             curr_word = ""
-    #         else:
-    #             curr_word += char
-
-    #     if has_num_name(curr_word):
-    #         curr_num = NUMBERS.index(get_num_name(curr_word))
-    #         nums.append(curr_num)
-    #         curr_word = ""
-    #     else:
-    #         curr_word += char
-
-    #     print(nums)
-    #     values.append(int(f"{nums[0]}{nums[-1]}"))
-    # print(sum(values))
-
-    # Part 2
-    values = []
+    values1 = []
+    values2 = []
     for line in lines:
+        # Part 1
+        only_nums = "".join([a for a in line if isnum(a)])
+        values1.append(int(only_nums[0] + only_nums[-1]))
+        # Part 2
         nums = []
         for idx in range(len(line)):
             num_name = ending_num_name(line[:idx+1])
@@ -90,12 +41,9 @@ def main():
                 nums.append(num_name)
             if isnum(line[idx]):
                 nums.append(int(line[idx]))
-        print(nums)
-        values.append(int(f"{nums[0]}{nums[-1]}"))
-    print(sum(values))
-
-
-
+        values2.append(int(f"{nums[0]}{nums[-1]}"))
+    print(sum(values1))
+    print(sum(values2))
 
 
 if __name__ == '__main__':
