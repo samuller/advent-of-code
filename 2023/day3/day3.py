56db59b6dc436c91cd546ea9db8e4e7cef055738
Simon Muller <samullers@gmail.com>
2023-12-03 10:07:31 +0200

    Add empty value to end for simpler handling

--- 2023/day3/day3.py
+++ 2023/day3/day3.py
@@ -27,17 +27,19 @@ def find_adjacent(schematic, row_idx, start, end):
     return adjacent
 
 
-# [7:20] 530002 [end of line]
-# [7:27] 533040 [end of line offsets end]
+# [7:20] 530002 [bug1: end of line]
+# [7:27] 533040 [bug2: end of line offsets end]
 # [7:27-7:34] delay 
 # [8:03] ponder
-# [8:20] 531167 [end of line offsets start]
+# [8:20] 531167 [bug3: end of line offsets start]
 # [8:25] finally
 def main():
     lines = [line.strip() for line in fileinput.input()]
 
     schematic = []
     for line in lines:
+        # Add empty value at end so we don't need extra handling for numbers at end of lines
+        line += '.'
         schematic.append(list(line))
         assert len(schematic[-1]) == len(schematic[0])
 
@@ -46,29 +48,23 @@ def main():
     for row_idx in range(len(schematic)):
         line = schematic[row_idx]
         curr_number = ""
-        for col_idx, val in enumerate(line):
-            if val.isdecimal():
-                curr_number += val
-            # If possible end of number
-            if not val.isdecimal() or col_idx == len(line) - 1:
-                end_idx = col_idx
-                # Workaround for bugs
-                if val.isdecimal() and col_idx == len(line) - 1:
-                    end_idx = col_idx + 1
-                if len(curr_number) > 0:
-                    adj = find_adjacent(schematic, row_idx, end_idx - len(curr_number), end_idx)
-                    # print(curr_number, adj)
-                    if len(adj) > 0:
-                        ans1 += int(curr_number)
-                    # Part 2
-                    for key, val in adj.items():
-                        if val == '*':
-                            gear_ratios[key].append(int(curr_number))
-                    curr_number = ""
+        for col_idx in range(len(line) ):
+            if line[col_idx].isdecimal():
+                curr_number += line[col_idx]
+            # else if end of number
+            elif len(curr_number) > 0:
+                adj = find_adjacent(schematic, row_idx, col_idx - len(curr_number), col_idx)
+                # print(curr_number, adj)
+                if len(adj) > 0:
+                    ans1 += int(curr_number)
+                # Part 2
+                for key, val in adj.items():
+                    if val == '*':
+                        gear_ratios[key].append(int(curr_number))
+                curr_number = ""
     print(ans1)
 
     # Part 2
-    # print(gear_ratios)
     ans2 = 0
     for key, gears in gear_ratios.items():
         if len(gears) != 2:
