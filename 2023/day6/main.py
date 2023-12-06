ce9feeb66699db6ae1fe3a3ae74a929c85d1859f
Simon Muller <samullers@gmail.com>
2023-12-06 07:16:53 +0200

    Clean-up

--- 2023/day6/main.py
+++ 2023/day6/main.py
@@ -1,19 +1,9 @@
 #!/usr/bin/env python3
 import fileinput
-# import sys; sys.path.append("../..")
-# from lib import *
 
 
-def main():
-    lines = [line.strip() for line in fileinput.input()]
-    # Part 1
-    times = [int(n) for n in lines[0].split(": ")[1].strip().split()]
-    distances = [int(n) for n in lines[1].split(": ")[1].strip().split()]
-    # Part 2
-    times = [int("".join(lines[0].split(": ")[1].strip().split()))]
-    distances = [int("".join(lines[1].split(": ")[1].strip().split()))]
-
-    ans1 = 1
+def calc(times, distances):
+    ans = 1
     for idx in range(len(distances)):
         time = times[idx]
         min_dist = distances[idx]
@@ -23,9 +13,21 @@ def main():
             # print(i, new_dist)
             if new_dist > min_dist:
                 ways_to_win += 1
-        print(ways_to_win)
-        ans1 *= ways_to_win
-    print(ans1)
+        # print(ways_to_win)
+        ans *= ways_to_win
+    return ans
+
+
+def main():
+    lines = [line.strip() for line in fileinput.input()]
+    # Part 1
+    times = [int(n) for n in lines[0].split(": ")[1].strip().split()]
+    distances = [int(n) for n in lines[1].split(": ")[1].strip().split()]
+    print(calc(times, distances))
+    # Part 2
+    times = [int("".join(lines[0].split(": ")[1].strip().split()))]
+    distances = [int("".join(lines[1].split(": ")[1].strip().split()))]
+    print(calc(times, distances))
 
 
 if __name__ == '__main__':
