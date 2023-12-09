fbdc288b0709ff1c666d3da0b64ede90856fd078
Simon Muller <samullers@gmail.com>
2023-12-09 09:09:50 +0200

    Clean-up day 8

--- 2023/day8/main.py
+++ 2023/day8/main.py
@@ -4,30 +4,18 @@ import fileinput
 import sys; sys.path.append("../..")
 from lib import *
 
-# steps = [13939, 17621, 19199, 15517, 12361, 20777]
-# print(steps)
-# new_steps = []
-# for step in steps:
-#     new_steps.append(step // 263)
-# print(new_steps)
-# print(prod(new_steps) * 263)
-# exit()
 
-# 2 3 5
-# 2*3*5 = 30
-# 6*3*5 = 90 -> 30
-# 2*3 * 3 * 5
-# 13939, 17621, 19199, 15517, 12361, 20777
-# 53 × 263, 67 × 263, 73 × 263, 59 × 263, 79 × 263
-# 53, 67, 73, 59, 79
-def follow_cycles(starts, instructions, nodes):
+def follow_multiple(starts, instructions, nodes):
+    """Follow multiple paths via brute-force.
+    
+    Doesn't work fast enough with given data, but more generally applicable to any data.
+    """
     currs = list(starts)
     steps_needed = [0] * len(currs)
 
     steps = 0
     ins_idx = 0
-    # while not all([curr[-1] == "Z" for curr in currs]):
-    while any([step == 0 for step in steps_needed]):
+    while not all([curr[-1] == "Z" for curr in currs]):
         ins = instructions[ins_idx % len(instructions)]
         ins_idx += 1
         for idx, curr in enumerate(currs):
@@ -47,16 +35,27 @@ def follow_cycles(starts, instructions, nodes):
         #     print(best_ans)
         steps += 1
 
-    # print(currs)
-    # print(steps_needed, prod(steps_needed))
+    print(currs)
+    print(steps_needed, prod(steps_needed))
     return math.lcm(*steps_needed)
 
 
-def follow(start, instructions, nodes):
+def follow_cycles(starts, instructions, nodes):
+    """Process data with expectation of proper cycles in graph."""
+    steps_needed = []
+
+    for start in starts:
+        steps = follow(start, instructions, nodes, lambda val: val[-1] == "Z")
+        steps_needed.append(steps)
+    # Can also take all prime factors, remove duplicates and multiple them all together.
+    return math.lcm(*steps_needed)
+
+
+def follow(start, instructions, nodes, is_end = lambda val: val == "ZZZ"):
     curr = start
     steps = 0
     ins_idx = 0
-    while curr != "ZZZ":
+    while not is_end(curr):
         ins = instructions[ins_idx % len(instructions)]
         ins_idx += 1
         if ins == "L":
@@ -66,7 +65,7 @@ def follow(start, instructions, nodes):
         else:
             assert False, ins
         steps += 1
-    assert curr == "ZZZ"
+    assert is_end(curr)
     # assert ins_idx == steps + 1
     return steps
 
