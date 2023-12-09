1f208b1da834621432b0f8759a76a693f6513090
Simon Muller <samullers@gmail.com>
2023-12-09 09:32:04 +0200

    Day 7 refactor into some functions

--- 2023/day7/main.py
+++ 2023/day7/main.py
@@ -25,76 +25,82 @@ card_ranks = ['A', 'K', 'Q', 'T', '9', '8', '7', '6', '5', '4', '3', '2', 'J']
 card_ranks.reverse()
 hand_ranks = []
 
+
+def replace_jokers(hand):
+    assert 'J' in hand
+    unique = Counter(hand)
+    new_hand = hand
+    other_cards = [c for c in hand if c != 'J']
+    # sort by value
+    other_cards.sort(key=lambda val: card_ranks.index(val))
+    count_others = Counter(other_cards)
+    # 5x Js are just lowest pentuplets
+    if unique['J'] == 4:
+        assert len(count_others) == 1
+        other_card = other_cards[0]
+        # highest other card - pents
+        new_hand = other_card*5
+    elif unique['J'] == 3:
+        assert len(count_others) in [1, 2]
+        # highest other card - quads
+        new_hand = hand.replace('J', other_cards[-1])
+    elif unique['J'] == 2:
+        assert len(count_others) in [1, 2, 3]
+        if len(count_others) == 3:
+            # highest other card - trips
+            new_hand = hand.replace('J', other_cards[-1])
+        elif len(count_others) == 2:
+            # most other cards - quads
+            joker_card = Counter(other_cards).most_common()[0][0]
+            new_hand = hand.replace('J', joker_card)
+        elif len(count_others) == 1:
+            # any - pents
+            new_hand = hand.replace('J', other_cards[0])
+    elif unique['J'] == 1:
+        assert len(count_others) in [1, 2, 3, 4]
+        if len(count_others) == 4:
+            # highest other card - pair
+            new_hand = hand.replace('J', other_cards[-1])
+        if len(count_others) == 3:
+            # most other cards - trips
+            # (or highest other card?)
+            joker_card = Counter(other_cards).most_common()[0][0]
+            new_hand = hand.replace('J', joker_card)
+        elif len(count_others) == 2:
+            # most other cards - quads or full house!!!
+            commons = count_others.most_common()
+            # 3/1 quads
+            if commons[0][1] == 3:
+                assert commons[1][1] == 1
+                # most other cards
+                joker_card = Counter(other_cards).most_common()[0][0]
+                new_hand = hand.replace('J', joker_card)
+            # 2/2 full house
+            elif commons[1][1] == 2:
+                assert commons[0][1] == 2
+                # highest other card
+                new_hand = hand.replace('J', other_cards[-1])
+            # quads?
+            else:
+                assert False
+                # joker_card = commons[0][0]
+                # new_hand = hand.replace('J', joker_card)
+        elif len(count_others) == 1:
+            # any - pents
+            new_hand = hand.replace('J', other_cards[0])
+    # print(hand, "->", new_hand)
+    return new_hand
+
+
 def value_of_hand(hand):
     assert len(hand) == 5
     for c in hand:
         assert c in card_ranks
     per_card_value = tuple([card_ranks.index(c) for c in hand])
-    unique = Counter(hand)
     # Part 2
     # TODO: full house
     if 'J' in hand:
-        new_hand = hand
-        other_cards = [c for c in hand if c != 'J']
-        # sort by value
-        other_cards.sort(key=lambda val: card_ranks.index(val))
-        count_others = Counter(other_cards)
-        # 5x Js are just lowest pentuplets
-        if unique['J'] == 4:
-            assert len(count_others) == 1
-            other_card = other_cards[0]
-            # highest other card - pents
-            new_hand = other_card*5
-        elif unique['J'] == 3:
-            assert len(count_others) in [1, 2]
-            # highest other card - quads
-            new_hand = hand.replace('J', other_cards[-1])
-        elif unique['J'] == 2:
-            assert len(count_others) in [1, 2, 3]
-            if len(count_others) == 3:
-                # highest other card - trips
-                new_hand = hand.replace('J', other_cards[-1])
-            elif len(count_others) == 2:
-                # most other cards - quads
-                joker_card = Counter(other_cards).most_common()[0][0]
-                new_hand = hand.replace('J', joker_card)
-            elif len(count_others) == 1:
-                # any - pents
-                new_hand = hand.replace('J', other_cards[0])
-        elif unique['J'] == 1:
-            assert len(count_others) in [1, 2, 3, 4]
-            if len(count_others) == 4:
-                # highest other card - pair
-                new_hand = hand.replace('J', other_cards[-1])
-            if len(count_others) == 3:
-                # most other cards - trips
-                # (or highest other card?)
-                joker_card = Counter(other_cards).most_common()[0][0]
-                new_hand = hand.replace('J', joker_card)
-            elif len(count_others) == 2:
-                # most other cards - quads or full house!!!
-                commons = count_others.most_common()
-                # 3/1 quads
-                if commons[0][1] == 3:
-                    assert commons[1][1] == 1
-                    # most other cards
-                    joker_card = Counter(other_cards).most_common()[0][0]
-                    new_hand = hand.replace('J', joker_card)
-                # 2/2 full house
-                elif commons[1][1] == 2:
-                    assert commons[0][1] == 2
-                    # highest other card
-                    new_hand = hand.replace('J', other_cards[-1])
-                # quads?
-                else:
-                    assert False
-                    # joker_card = commons[0][0]
-                    # new_hand = hand.replace('J', joker_card)
-            elif len(count_others) == 1:
-                # any - pents
-                new_hand = hand.replace('J', other_cards[0])
-        print(hand, "->", new_hand)
-        hand = new_hand
+        hand = replace_jokers(hand)
     unique = Counter(hand)
     most = unique.most_common()[0]
     common = unique.most_common()
@@ -144,14 +150,7 @@ def value_of_hand(hand):
 assert value_of_hand("A2345") == (1, 12, 1, 2, 3, 4), value_of_hand("A2345")
 
 
-# [7:23] 254142052 [highest card value]
-# [7:28] 253677864 [wrong draw ranking]
-# [7:35] part1!
-def main():
-    lines = [line.strip() for line in fileinput.input()]
-
-    print(card_ranks)
-
+def total_winnings(lines):
     hand_to_value = dict()
     hand_to_bid = dict()
     for line in lines:
@@ -170,7 +169,19 @@ def main():
     for rank, hand_value in enumerate(hand_values):
         hand, value = hand_value
         bid = hand_to_bid[hand]
-        ans1 += (bid * (rank + 1)) 
+        ans1 += (bid * (rank + 1))
+    return ans1
+
+
+# [7:23] 254142052 [highest card value]
+# [7:28] 253677864 [wrong draw ranking]
+# [7:35] part1!
+def main():
+    lines = [line.strip() for line in fileinput.input()]
+
+    print(card_ranks)
+
+    ans1 = total_winnings(lines)
     print(ans1)
     # print(hands_value)
 
