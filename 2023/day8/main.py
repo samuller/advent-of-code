8eebc80f520e94bcdfdf81bff047a6088a707a8c
Simon Muller <samullers@gmail.com>
2023-12-09 07:31:18 +0200

    Day 8 part 2

--- 2023/day8/main.py
+++ 2023/day8/main.py
@@ -1,22 +1,89 @@
 #!/usr/bin/env python3
 import fileinput
-# import sys; sys.path.append("../..")
-# from lib import *
+import sys; sys.path.append("../..")
+from lib import *
+
+# steps = [13939, 17621, 19199, 15517, 12361, 20777]
+# print(steps)
+# new_steps = []
+# for step in steps:
+#     new_steps.append(step // 263)
+# print(new_steps)
+# print(prod(new_steps) * 263)
+# exit()
+
+# 2 3 5
+# 2*3*5 = 30
+# 6*3*5 = 90 -> 30
+# 2*3 * 3 * 5
+# 13939, 17621, 19199, 15517, 12361, 20777
+# 53 × 263, 67 × 263, 73 × 263, 59 × 263, 79 × 263
+# 53, 67, 73, 59, 79
+def follow2(starts, instructions, nodes):
+    currs = list(starts)
+    steps_needed = [0] * len(currs)
+
+    steps = 0
+    ins_idx = 0
+    best_ans = 10^20
+    while not all([curr[-1] == "Z" for curr in currs]):
+    # while any([step == 0 for step in steps_needed]):
+        ins = instructions[ins_idx % len(instructions)]
+        ins_idx += 1
+        # print(currs)
+        # print(ins)
+        for idx, curr in enumerate(currs):
+            if ins == "L":
+                currs[idx] = nodes[curr][0]
+            elif ins == "R":
+                currs[idx] = nodes[curr][1]
+            else:
+                assert False, ins
+            # assert currs[idx] != "XXX", currs
+            if currs[idx][-1] == "Z": # and steps_needed[idx] == 0:
+                steps_needed[idx] = steps + 1
+                print(steps_needed, prod(steps_needed), prod(steps_needed) < best_ans)
+        if prod(steps_needed) != 0 and prod(steps_needed) < best_ans:
+            best_ans = prod(steps_needed)
+            print(best_ans)
+        steps += 1
+        # if steps == 12500:
+        #     exit()
+    # assert currs[-1] == "Z"
+    print(currs)
+    print(steps_needed, prod(steps_needed))
+    # assert ins_idx == steps + 1
+    return steps
 
 
-class Classy:
-    def __init__(self):
-        pass
+def follow(start, instructions, nodes):
+    curr = start
+    steps = 0
+    ins_idx = 0
+    while curr != "ZZZ":
+        ins = instructions[ins_idx % len(instructions)]
+        ins_idx += 1
+        if ins == "L":
+            curr = nodes[curr][0]
+        elif ins == "R":
+            curr = nodes[curr][1]
+        else:
+            assert False, ins
+        steps += 1
+    assert curr == "ZZZ"
+    # assert ins_idx == steps + 1
+    return steps
 
 
-def function(input):
-    return False
-
-
+# [7:27] 18792518403772666320570269
+# [7:38] 18792518403772666320570269 // 263 (GCD) = 71454442599896069659963
+# [7:44] 56787204941 (multiplied without 263s, e.g. // 263^5)
+# [7:45] correct (multiplied with one 263)
 def main():
     lines = [line.strip() for line in fileinput.input()]
 
     instructions = lines[0]
+    part2_starts = []
     assert lines[1] == ""
     nodes = dict()
     for line in lines[2:]:
@@ -24,26 +91,20 @@ def main():
         # Remove brackets
         neigh = neigh[1:-1]
         left, right = neigh.split(", ")
-        print(node, left, right)
+        # print(node, left, right)
+        assert node not in nodes
         nodes[node] = (left, right)
-    print(nodes)
-    curr = "AAA"
-    ans1 = 0
-    ins_idx = 0
-    while curr != "ZZZ":
-        ins = instructions[ins_idx % len(instructions)]
-        ins_idx += 1
-    # for ins in enuminstructions:
-        print("INS", ins)
-        if ins == "L":
-            curr = nodes[curr][0]
-        elif ins == "R":
-            curr = nodes[curr][1]
-        else:
-            assert False, ins
-        ans1 += 1
-    assert curr == "ZZZ"
-    print(ans1)
+        if node[-1] == "A":
+            part2_starts.append(node)
+
+    # print(nodes)
+    # print(part2_starts)
+
+    # ans1 = follow("AAA", instructions, nodes)
+    # print(ans1)
+    ans2 = follow2(part2_starts, instructions, nodes)
+    print(ans2)
+
 
 if __name__ == '__main__':
     main()
