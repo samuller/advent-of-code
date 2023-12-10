dc26982c5f7ca1da676cb810e830550e9f92749f
Simon Muller <samullers@gmail.com>
2023-12-10 10:35:03 +0200

    Remove unused code

--- 2023/day10/main.py
+++ 2023/day10/main.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python3
-import itertools
 import fileinput
-from enum import Enum
-# import sys; sys.path.append("../..")
-# from lib import *
 
 
 NORTH = (-1, 0)
@@ -11,10 +7,7 @@ SOUTH = (1, 0)
 WEST = (0, -1)
 EAST = (0, 1)
 
-
-# DR=[-1,0,1,0]
-# DC=[0,-1,0,1]
-DRDC = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+DRDC = [NORTH, EAST, SOUTH, WEST]
 
 PIPE_CONNECT = {
     # | is a vertical pipe connecting north and south.
@@ -35,65 +28,6 @@ PIPE_CONNECT = {
 
 def rel_to_abs(abs_ref, rel):
     return abs_ref[0] + rel[0], abs_ref[1] + rel[1]
-
-
-def get_next(pipe, dr, dc):
-    # | is a vertical pipe connecting north and south.
-    # - is a horizontal pipe connecting east and west.
-    # L is a 90-degree bend connecting north and east.
-    # J is a 90-degree bend connecting north and west.
-    # 7 is a 90-degree bend connecting south and west.
-    # F is a 90-degree bend connecting south and east.
-    # North
-    if (dr, dc) == (-1, 0):
-        if pipe in ['|', 'J', 'L']:
-            match pipe:
-                case '|':
-                    return (-2, 0)
-                case 'J':
-                    return (-1, -1)
-                case 'L':
-                    return (-1,  1)
-        else:
-            return None
-    # South
-    elif (dr, dc) == (1, 0):
-        if pipe in ['|', '7', 'F']:
-            match pipe:
-                case '|':
-                    return (2,  0)
-                case '7':
-                    return (1, -1)
-                case 'F':
-                    return (1,  1)
-        else:
-            return None
-    # West
-    elif (dr, dc) == (0, -1):
-        if pipe in ['-', 'L', 'F']:
-            match pipe:
-                case '-':
-                    return ( 0, -2)
-                case 'L':
-                    return (-1, -1)
-                case 'F':
-                    return ( 1, -1)
-        else:
-            return None
-    # East
-    elif (dr, dc) == (0, 1):
-        if pipe in ['-', 'J', '7']:
-            match pipe:
-                case '-':
-                    return ( 0, 2)
-                case 'J':
-                    return (-1, 1)
-                case '7':
-                    return ( 1, 1)
-        else:
-            return None
-    else:
-        assert False
 
 
 def follow_path(grid, start):
@@ -146,27 +80,7 @@ def follow_path(grid, start):
             look_dirs = PIPE_CONNECT[new_pipe]
 
             added.append((lr, lc))
-            # if len(path) > 10:
-            #     exit()
             break
-
-            # nxt = get_next(pipe1, dr, dc)
-            # print(f"{(dr, dc)} / {pipe1} -> {nxt}")
-            # if nxt is not None:
-            #     nr, nc = r1 + nxt[0], c1 + nxt[1]
-            #     if (nr, nc) == start:
-            #         path.append((lr, lc))
-            #         return path
-            #     # If out of bounds
-            #     if not ((0 <= nr < R) and (0 <= nc < C)):
-            #         continue
-            #     # Take first direction we haven't been to yet
-            #     if (nr, nc) in path:
-            #         continue
-            #     path.append((lr, lc))
-            #     path.append((nr, nc))
-            #     added.append((lr, lc))
-            #     break
         assert len(added) != 0, (lr, lc)
         # print(path)
 
@@ -221,67 +135,7 @@ def main():
                 enclosed += 1
     # 9:54 - wrong:  67
     print(enclosed)
-
-
-
-    # # seen = set(start)
-    # path1 = [start]
-    # # path2 = [start]
-    # while True:
-    #     two_paths = []
-    #     r1, c1 = path1[-1]
-    #     r2, c2 = path2[-1]
-    #     # next1 = [get_next(grid[r1+dr][c1+dc], dr, dc) for (dr, dc) in DRDC]
-    #     # next2 = [get_next(grid[r2+dr][c2+dc], dr, dc) for (dr, dc) in DRDC]
-    #     # print(next1)
-    #     # print(next2)
-    #     for i in range(4):
-    #         dr, dc = DRDC[i] #DR[i], DC[i]
-    #         # look
-    #         lr, lc = r1+dr, c1+dc
-    #         if (lr, lc) == start:
-    #             exit()
-    #         pipe1 = grid[lr][lc]
-    #         nxt1 = get_next(pipe1, dr, dc)
-    #         print(f"{(dr, dc)} / {pipe1} -> {nxt1}")
-    #         if nxt1 is not None:
-    #             nr, nc = nxt1
-    #             nxt_abs = r1 + nr, c1 + nc
-    #             assert nxt_abs not in path1
-    #             path1.append((lr, lc))
-    #             path1.append(nxt_abs)
-    #             break
-    #             # seen.add(nxt)
-    #         # pipe2 = grid[r2+dr][c2+dc]
-    #         # nxt2 = get_next(pipe2, dr, dc)
-    #         # if nxt2 is not None:
-    #         #     assert nxt2 not in path2
-    #         #     path1.append((dr, dc))
-    #         #     path1.append(nxt2)
-    #     print(path1)
-    #     print(path2)
-
-    #     # for i in range(4):
-    #     #     dr, dc = DR[i], DC[i]
-    #     #     # if (dr, dc) == (0, 0):
-    #     #     #     continue
-    #     #     if (dr, dc) in seen:
-    #     #         continue
-    #     #     pipe = grid[r1+dr][c1+dc]
-    #     #     # . is ground; there is no pipe in this tile.
-    #     #     if pipe == '.':
-    #     #         continue
-    #     #     nxt1 = get_next(pipe, r1 + dr, c1 + dc)
-    #     #     nxt2 = get_next(pipe, r2 + dr, c2 + dc)
-    #     #     if nxt1 is not None:
-    #     #         curr1 = nxt1
-    #     #     if nxt2 is not None:
-    #     #         curr2 = nxt2
-    #         # match (dr, dc), val:
-    #         #     case ()
-    #         # If connected to current:
-    #     # seen.add(curr1)
-    #     # seen.add(curr2)
+    # https://stackoverflow.com/questions/217578/how-can-i-determine-whether-a-2d-point-is-within-a-polygon
 
 
 if __name__ == '__main__':
