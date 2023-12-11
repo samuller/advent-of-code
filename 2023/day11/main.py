2ee68458610f5f2a5b39447069d64132ace0aa8e
Simon Muller <samullers@gmail.com>
2023-12-11 07:49:29 +0200

    Clean-up and combine parts

--- 2023/day11/main.py
+++ 2023/day11/main.py
@@ -3,6 +3,25 @@ import fileinput
 import itertools
 
 
+def examples(g1, g2, dist):
+    if (g1, g2) == ((5, 1), (9, 4)):
+        # 1st example
+        assert dist == 9, dist
+    if (g1, g2) == ((0, 3), (8, 7)):
+        # 2nd example
+        assert dist == 15, dist
+    if (g1, g2) == ((2, 0), (6, 9)):
+        # 3nd example
+        assert dist == 17, dist
+    if (g1, g2) == ((9, 0), (9, 4)):
+        # 4th example
+        assert dist == 5, dist
+
+
+# Part 1:
+# - delayed with typo in expansion ("< col <" was row)
+#   - but I investigated adding 1 to Manhattan distances...
+#   - had to add all example data as asserts
 # Part 2:
 # [7:41] not 779033026240 [multiplication vs addition]
 def main():
@@ -35,38 +54,29 @@ def main():
     # print(galaxies)
 
     ans1 = 0
-    expansion = 2
+    ans2 = 0
+    expansion = [2, 1000000]
     for g1, g2 in itertools.combinations(galaxies, 2):
         assert g1 != g2
         # Manhattan distance
         dist = abs(g1[0] - g2[0]) + abs(g1[1] - g2[1])
-        # if g1[0] != g2[0]:
-        #     dist += 1
-        # if g1[1] != g2[1]:
-        #     dist += 1
+        dist2 = dist
         for row in empty_rows:
             if min(g1[0], g2[0]) < row < max(g1[0], g2[0]):
-                dist += (expansion - 1)
+                dist += (expansion[0] - 1)
+                dist2 += (expansion[1] - 1)
                 # print("Row", row)
         for col in empty_cols:
             if min(g1[1], g2[1]) < col < max(g1[1], g2[1]):
-                dist += (expansion - 1)
+                dist += (expansion[0] - 1)
+                dist2 += (expansion[1] - 1)
                 # print("Col", col)
         # print(g1, g2, dist)
-        # if (g1, g2) == ((5, 1), (9, 4)):
-        #     # 1st example
-        #     assert dist == 9, dist
-        # if (g1, g2) == ((0, 3), (8, 7)):
-        #     # 2nd example
-        #     assert dist == 15, dist
-        # if (g1, g2) == ((2, 0), (6, 9)):
-        #     # 3nd example
-        #     assert dist == 17, dist
-        # if (g1, g2) == ((9, 0), (9, 4)):
-        #     # 4th example
-        #     assert dist == 5, dist
+        # examples(g1, g2, dist)
         ans1 += dist
+        ans2 += dist2
     print(ans1)
+    print(ans2)
 
 
 if __name__ == '__main__':
