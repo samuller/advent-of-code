79a09bfb5aae49065164a1fb6b0b8ba574060cfe
Simon Muller <samullers@gmail.com>
2023-12-12 17:47:45 +0200

    Day 12  part 2 alternatives

--- 2023/day12/main.py
+++ 2023/day12/main.py
@@ -33,17 +33,128 @@ assert calc_data([c for c in ".###.##...#.#.#"]) == [3,2,1,1,1]
 def inter_possibilities(springs, data):
     springs = springs + ['?'] + list(springs)
     data = data + list(data)
-    ans = possibilities(springs, data)
+    ans = count_possibilities_bruteforce(springs, data)
     return ans
 
 # assert inter_possibilities([c for c in "?###????????"], [3,2,1]) == 10
 
 
-def possibilities(springs, data):
+def valid_gaps(data, spring_count):
+    assert sum(data)+(len(data) - 1) <= spring_count, f"{sum(data)}+{(len(data) - 1)} < {spring_count}"
+    # endpoints + in-between
+    gaps = 2 + (len(data) - 1)
+    # Gaps between data numbers, but excluding mandatory in-between gaps
+    gap_count = spring_count - sum(data) - (len(data) - 1)
+    # print(gap_count)
+    # generate variations of "gaps"x numbers that sum to spring_count
+    for variant in itertools.product(range(gap_count+1), repeat=gaps):
+        # print(variant)
+        if (sum(variant) == gap_count):
+            yield variant
+
+
+def count_possibilities_matching_gaps(springs, data):
     print(springs, data)
-    counts = Counter(springs)
     ans = 0
+
+    # known_springs
+
+    for variant in valid_gaps(data, len(springs)):
+        # Check if gap variation matches known springs
+        for idx, dt in enumerate(data):
+            pass
+
+    #     valid = True
+    #     for idx, spring in enumerate(springs):
+    #         if spring == '?':
+    #             continue
+    #         elif poss[idx] != spring:
+    #             valid = False
+    #             break
+    #     if valid:
+    #         ans += 1
+
+    # ans = math.pow(2, counts.get('?', 0))
+    # print(ans)
+    return ans
+
+
+def valid_possibilities(data, spring_count):
+    for variant in valid_gaps(data, spring_count):
+        # print(variant)
+        # Generate springs from data and gaps variation
+
+        # Slow
+        # poss = []
+        # for idx, num in enumerate(variant):
+        #     poss.extend(["."]*num)
+        #     if idx != len(variant)-1:
+        #         poss.extend(["#"]*data[idx])
+        #         if idx != len(variant)-2:
+        #             poss.append(".")
+        # assert len(poss) == spring_count, len(poss)
+
+        # Faster?
+        poss = ['?']*spring_count
+        pidx = 0
+        for idx, num in enumerate(variant):
+            for _ in range(num):
+                poss[pidx] = "."
+                pidx += 1
+            if idx != len(variant)-1:
+                for _ in range(data[idx]):
+                    poss[pidx] = "#"
+                    pidx += 1
+                if idx != len(variant)-2:
+                    poss[pidx] = "."
+                    pidx += 1
+        # assert len(poss) == spring_count, len(poss)
+
+        # print(variant, poss)
+        yield poss
+
+
+# assert valid_possibilities([1,2,3], 10)
+
+
+# 128 -> 1
+# 2048 -> 38760
+# 131072 -> 45
+# 512 -> 8008
+# 512 -> 8008
+# 524288 -> 3003
+# 2.6s -> 14.8s
+def count_possibilities_only_valid(springs, data):
+    print(springs, data)
+    ans = 0
+    count = 0
+    for poss in valid_possibilities(data, len(springs)):
+        # Check variation is valid with known data
+        count += 1
+        valid = True
+        for idx, spring in enumerate(springs):
+            if spring == '?':
+                continue
+            elif poss[idx] != spring:
+                valid = False
+                break
+        if valid:
+            ans += 1
+    print(count)
+    # ans = math.pow(2, counts.get('?', 0))
+    # print(ans)
+    return ans
+
+
+def count_possibilities_bruteforce(springs, data):
+    print(springs, data)
+    ans = 0
+
+    counts = Counter(springs)
+    count = 0
     for poss in itertools.product(('.', '#'), repeat=counts.get('?', 0)):
+        count += 1
+        # Generate valid variation from permutation
         imagine = list(springs)
         # print(poss)
         pidx = 0
@@ -54,15 +165,16 @@ def possibilities(springs, data):
         if calc_data(imagine) == data:
             # print(imagine)
             ans += 1
+    print(count)
     # ans = math.pow(2, counts.get('?', 0))
+    # print(ans)
     return ans
 
-assert possibilities([c for c in "?###????????"], [3,2,1]) == 10
+assert count_possibilities_bruteforce([c for c in "?###????????"], [3,2,1]) == 10
 
 
 def main():
     lines = [line.strip() for line in fileinput.input()]
-    print(f'Lines: {len(lines)}')
 
     ans1 = 0
     ans2 = 0
@@ -71,10 +183,28 @@ def main():
         springs = [c for c in springs]
         data = [int(d) for d in data.split(",")]
         assert set(springs) - set(['#', '.', '?']) == set()
-        # ans1 += possibilities(springs, data)
-        # ans2 += possibilities(springs * 5, data * 5)
-        ans2 += possibilities(springs, data) * 5
-        ans2 += inter_possibilities(springs, data) * 4
+
+        func = count_possibilities_bruteforce
+        # func = count_possibilities_only_valid
+
+        ans1 += count_possibilities_only_valid(springs, data)
+        continue
+        repeat = 5
+        unfolded = [*springs, '?'] * repeat
+        unfolded = unfolded[:-1]
+
+        # counts = Counter(unfolded)
+        # if counts['?'] > len(unfolded) or counts['?'] > 16:
+        #     func = count_possibilities_only_valid
+        # print(' '*6, func.__name__, counts['?'], len(unfolded))
+
+        # ans2 += count_possibilities_bruteforce(unfolded, data * 2)
+        ans2 += func(unfolded, data * repeat)
+        # unfolded = [*springs, '?'] * 5
+        # unfolded = unfolded[:-1]
+        # ans2 += count_possibilities(unfolded, data * 5)
+        # ans2 += count_possibilities(springs, data) * 5
+        # ans2 += inter_possibilities(springs, data) * 4
     print(ans1)
     print(ans2)
 
